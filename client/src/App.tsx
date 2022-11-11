b25cc2fa1eac2ab6b20f55c101c3c383edf686c4
headring <myhakiki@gmail.com>
2022-11-11 16:32:18 +0900

    feat: 초기설정(ts, react-router-dom, styled-components, recoil, axios, eslint, prettier)

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import Some from './some';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>hi</div>
+        <Some />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/some.js
@@ -0,0 +1,9 @@
+const Some = () => {
+  return (
+    <>
+      <div>click</div>
+    </>
+  );
+};
+
+export default Some;
