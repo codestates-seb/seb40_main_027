1cb8bedfe347f21599f251515abfcb9b2f724b87
headring <myhakiki@gmail.com>
2022-11-14 16:35:52 +0900

    style: searchBar 스타일 적용 중

--- client/src/components/SearchBar/index.tsx
+++ client/src/components/SearchBar/index.tsx
@@ -1,11 +1,36 @@
 import { Icon } from '@iconify/react';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const SearchWrapper = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  input {
+    padding-left: 1rem;
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    font-size: 80%;
+    :focus {
+      outline: #00ff00 dotted thick;
+    }
+  }
+`;
 
 const SearchBar = () => {
+  const [searchText, setSearchText] = useState('');
+  const handleChange = (e: any) => setSearchText(e.target.value);
+  // api 연동 시 검색이 가능한 onSubmit 필요
+
   return (
-    <div>
+    <SearchWrapper>
       <Icon icon="ant-design:search-outlined" />
-      <span>hi</span>
-    </div>
+      <input type="text" className="search-bar" placeholder="검색" value={searchText} onChange={handleChange}></input>
+    </SearchWrapper>
   );
 };
 export default SearchBar;
