aca6a432a496387d9482e4da1da17c05060c5730
headring <myhakiki@gmail.com>
2022-11-14 15:58:58 +0900

    feat:searchBar 브랜치 생성 및 iconify 작동 확인

---
+++ client/src/components/SearchBar/index.tsx
@@ -0,0 +1,11 @@
+import { Icon } from '@iconify/react';
+
+const SearchBar = () => {
+  return (
+    <div>
+      <Icon icon="ant-design:search-outlined" />
+      <span>hi</span>
+    </div>
+  );
+};
+export default SearchBar;
