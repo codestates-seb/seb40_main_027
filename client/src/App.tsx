81246179b812324a441d7d1103bf9fc57f26a7e1
skyhanull <anky0425@naver.com>
2022-11-14 10:57:49 +0900

    feat:header추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import Some from './some';
+// import logo from './image/logo.png';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <Header />
+          {/* <img src={logo} alt="logo" /> */}
+        </div>
         <div>hi</div>
         <Some />
       </header>
