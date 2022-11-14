a080bf200dc45a7126b7630f5453fc5ec5f8245b
didwndbs <didwndbs@naver.com>
2022-11-14 14:44:51 +0900

    Revert "feat: #12-mentoring게시판 구현"
    This reverts commit 3d92d5b6d79b8ef02b54bec04f93d482cb20c24d.

--- server/src/main/java/com/yes27/ServerApplication.java
+++ server/src/main/java/com/yes27/ServerApplication.java
@@ -2,10 +2,8 @@ package com.yes27;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ServerApplication {
 
 	public static void main(String[] args) {
