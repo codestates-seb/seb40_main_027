100c78e47a86871f4f3f51a0e0f8382b6baca7fc
pjnw1236 <pjnw1236@gmail.com>
2022-11-14 14:08:31 +0900

    feat: configure basic spring security

---
+++ server/src/main/java/com/yes27/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.yes27.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .anyRequest()
+            .permitAll();
+
+        return http.build();
+    }
+}
