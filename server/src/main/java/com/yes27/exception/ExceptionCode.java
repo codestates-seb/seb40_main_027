5b867962c96d8c6fbd7c7553c1e6348b94afbfbc
youngjun kim <108052178+kyj4327@users.noreply.github.com>
2022-11-14 20:28:34 +0900

    fix : ExceptionCode 충돌 해결

--- server/src/main/java/com/yes27/exception/ExceptionCode.java
+++ server/src/main/java/com/yes27/exception/ExceptionCode.java
@@ -7,8 +7,13 @@ public enum ExceptionCode {
     STUDY_EXISTS(409, "Study exists"),
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
+
+    POSTSCRIPT_NOT_FOUND(404, "Postscript not found"),
+    POSTSCRIPT_EXISTS(409, "Postscript exists"),
+
     MENTOR_NOT_FOUND(404, "Mentor not found"),
     MENTOR_EXISTS(409, "Mentor exists");
+
 
     @Getter
     private int status;
