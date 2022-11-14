bdfe2e5cb208ee1f481797493aba6570699805eb
youngjun kim <108052178+kyj4327@users.noreply.github.com>
2022-11-14 17:10:01 +0900

    fix : SingleResponseDto 충돌 해결 #15

--- server/src/main/java/com/yes27/response/SingleResponseDto.java
+++ server/src/main/java/com/yes27/response/SingleResponseDto.java
@@ -3,8 +3,8 @@ package com.yes27.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
+@Getter
 public class SingleResponseDto<T> {
     private T data;
 }
