bdef2ed5ef16ab36130163a65cfcc578666542ed
Jongwon Park <jwp7073@gmail.com>
2022-11-14 10:59:37 +0900

    fix: App.tsx에서 some.js 관련 코드 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
-import Some from './some';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>hi</div>
-        <Some />
       </header>
     </div>
   );
