38982485e0f2f79e4cbcc4c081d920bde4be6f83
skyhanull <anky0425@naver.com>
2022-11-14 13:48:06 +0900

    design:nickname css수정

--- client/src/components/Nickname.tsx
+++ client/src/components/Nickname.tsx
@@ -1,9 +1,21 @@
+import styled from 'styled-components';
+
+const Imgcircle = styled.span`
+  height: 4rem;
+
+  .userimg {
+    width: 2rem;
+    height: 2rem;
+    border: 1px solid red;
+    border-radius: 50%;
+  }
+`;
 const Nickname = () => {
   return (
-    <div>
-      <span></span>
+    <Imgcircle>
+      <span className="userimg">사</span>
       <span>Nickname</span>
-    </div>
+    </Imgcircle>
   );
 };
 
