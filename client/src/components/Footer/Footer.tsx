cfc721aabfab451437c22ef1c2c54762d084b2e9
headring <myhakiki@gmail.com>
2022-11-14 11:47:47 +0900

    feat:footer branch 생성 및 시작

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const FooterWrapper = styled.div`
+  max-width: 1160px;
+`;
+
+const Footer: any = () => {
+  return (
+    <FooterWrapper>
+      <div>hi</div>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
