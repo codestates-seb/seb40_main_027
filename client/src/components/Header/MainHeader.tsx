eaa6c10b2e52170a3e7cf82152b7213975191a7e
Jongwon Park <jwp7073@gmail.com>
2022-11-15 12:31:04 +0900

    fix: logo 파일 경로 수정

--- client/src/components/Header/MainHeader.tsx
+++ client/src/components/Header/MainHeader.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import logo from '../image/logo.png';
+import logo from '../../image/logo.png';
 import { useState } from 'react';
 import Nickname from './Nickname';
 
