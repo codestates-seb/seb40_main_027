a1426cf579cebe19458856602f5cc9aae6de9666
Jongwon Park <jwp7073@gmail.com>
2022-11-15 12:32:31 +0900

    feat: react-router-dom 초기 세팅

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,16 @@ import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
