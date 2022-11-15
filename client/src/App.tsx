89ec40001149b66e9608326c60af659d8bffe00c
skyhanull <anky0425@naver.com>
2022-11-16 01:46:44 +0900

    feat:mypage router생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Mypage from './pages/Mypage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/users/me" element={<Mypage />} />
     </Routes>
   );
 }
