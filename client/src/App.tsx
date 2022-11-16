915adcf70de4f1b5ddf6e1310174d5b2aaddb771
headring <myhakiki@gmail.com>
2022-11-16 09:51:16 +0900

    feat: login router 연결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import Main from './pages/Main';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/user/login" element={<Login />} />
     </Routes>
   );
 }
