3f3a0a794b95c1890cac663920f02ee006ab7515
skyhanull <anky0425@naver.com>
2022-11-16 22:05:31 +0900

    feat:mypage구현

--- client/src/pages/Mypage.tsx
+++ client/src/pages/Mypage.tsx
@@ -1,14 +1,20 @@
 import PageHeader from '../components/Header/PageHeader';
-import Footer from '../components/Footer/Footer';
+// import MainHeader from '../components/Header/MainHeader';
+
 import styled from 'styled-components';
-import icon from '../image/icon.png';
+import icon from '../assets/image/icon.png';
 import { useState } from 'react';
 import MypageSchedule from '../components/MypageSchedule';
 import MypageList from '../components/MypageList';
+import { Icon } from '@iconify/react';
+
+const PageSize = styled.div`
+  overflow-y: auto;
+`;
 
 const MypageContent = styled.div`
-  height: 90vh;
-  margin: 0rem 10rem 0rem 10rem;
+  height: 100vh;
+  margin: 0rem 18% 0rem 18%;
 
   .tab-menu {
     height: 10%;
@@ -17,6 +23,9 @@ const MypageContent = styled.div`
     justify-content: space-between;
     align-items: flex-end;
     margin: 0rem 5rem 0rem 5rem;
+  }
+  .tab-content-mobile {
+    display: none;
   }
   .page-name {
     font-size: 2em;
@@ -42,27 +51,114 @@ const MypageContent = styled.div`
   .tab-body {
     height: 10%;
     background-color: var(--greenMain);
-    border-radius: 20px 20px 0px 0px;
+    border-radius: 40px 40px 0px 0px;
   }
   .mypage-body {
     display: flex;
-    flex-grow: 1;
-    /* flex-direction: row; */
+    border: 1px solid var(--grayContentsBorder);
   }
   .profile {
     width: 25%;
-    border: 1px solid var(--grayContentsBorder);
-    height: 40rem;
+    border-right: 1px solid var(--grayContentsBorder);
     display: flex;
   }
   .user-profile {
     height: 40%;
     border: 1px solid var(--grayContentsBorder);
-    margin: 0rem 1rem 0rem 1rem;
+    margin: 1rem 1rem 0rem 1rem;
     width: 100%;
   }
-  .maypage-view {
+  .mypage-view {
     width: 100%;
+  }
+  .icon-gear {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+  .user-picture {
+    display: flex;
+    width: 100%;
+    height: 60%;
+    background-color: red;
+  }
+  .user-info {
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+    justify-content: center;
+    flex-direction: column;
+    height: 30%;
+    align-items: center;
+  }
+  .user-nickname {
+    border: 1px solid var(--grayContentsBorder);
+    border-radius: 20px;
+    width: 70%;
+    margin-top: 0.5rem;
+  }
+
+  @media screen and (max-width: 414px) {
+    margin: 0px;
+
+    .tab-menu {
+      display: none;
+    }
+    .tab-content-mobile {
+      display: flex;
+      justify-content: flex-end;
+      margin-top: 1rem;
+    }
+    .page-name {
+      display: none;
+    }
+    .icon-wapper {
+      display: none;
+    }
+    img {
+      display: none;
+    }
+    .tab-body {
+      display: none;
+    }
+    .mypage-body {
+      margin: 0px;
+      display: flex;
+      width: 100vw;
+
+      flex-direction: column;
+      justify-content: center;
+      border: none;
+    }
+    .profile {
+      width: 100vw;
+      border: none;
+      display: flex;
+      justify-content: center;
+      z-index: 1;
+    }
+    .user-profile {
+      width: 100vw;
+      display: flex;
+      flex-direction: row;
+      height: 7rem;
+      border-radius: 20px;
+      margin: 1rem;
+    }
+    .user-picture {
+      height: 100%;
+    }
+    .user-info {
+      height: 100%;
+      width: 100%;
+    }
+    .mypage-view {
+    }
+    .tab-mobile-bar {
+      display: flex;
+      background-color: var(--greenMain);
+      height: 3rem;
+      width: 100%;
+    }
   }
 `;
 
@@ -86,15 +182,15 @@ const Mypage = () => {
     { id: 1, text: '나의 작성글' },
   ];
   return (
-    <div>
+    <PageSize>
       <PageHeader />
+
       <MypageContent>
         <div className="tab-menu">
           <span className="page-name">Mypage</span>
           <div className="tab-content">
             {TabMenu.map((el) => (
               <ButtonTab
-                // className="tab-button"
                 key={el.id}
                 onClick={() => TabHandler(el.id)}
                 // style={currentTab === el.id ?  : {}}
@@ -113,19 +209,38 @@ const Mypage = () => {
         <div className="mypage-body">
           <div className="profile">
             <div className="user-profile">
-              <span></span>
-              <div>
-                <div>nickname</div>
-                <div>email</div>
+              <div className="icon-gear">
+                <Icon icon="ph:gear-six-duotone" width="25" height="25" />
+              </div>
+              <div className="user-picture">사진</div>
+              <div className="user-info">
+                <div className="user-nickname">nickname</div>
+                <div className="user-nickname">email</div>
               </div>
             </div>
-            <div></div>
           </div>
-          <div className="maypage-view">{currentTab === 1 ? <MypageList /> : <MypageSchedule />}</div>
+          <div>
+            {' '}
+            <div className="tab-content-mobile">
+              {TabMenu.map((el) => (
+                <ButtonTab
+                  key={el.id}
+                  onClick={() => TabHandler(el.id)}
+                  className={currentTab === el.id ? 'tab-button' : 'tab-not-select'}
+                >
+                  {el.text}
+                </ButtonTab>
+              ))}
+            </div>
+            <div className="tab-mobile-bar"></div>
+          </div>
+          <div className="mypage-view">{currentTab === 1 ? <MypageList /> : <MypageSchedule />}</div>
         </div>
+        {/* <div className="footer-position">
+          <Footer />
+        </div> */}
       </MypageContent>
-      <Footer />
-    </div>
+    </PageSize>
   );
 };
 export default Mypage;
