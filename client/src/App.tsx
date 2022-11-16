292792b368e3d32b5551e22ff5bae6e602e8278e
skyhanull <anky0425@naver.com>
2022-11-16 22:03:33 +0900

    docs:footer추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 import Main from './pages/Main';
 import Mypage from './pages/Mypage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/users/me" element={<Mypage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/users/me" element={<Mypage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
