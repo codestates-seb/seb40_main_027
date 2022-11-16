3fbd99e19515d39ca8021088482958b5223f9ab2
skyhanull <anky0425@naver.com>
2022-11-16 22:03:57 +0900

    feat:mypage 작성글 페이지 구현

--- client/src/components/MypageList.tsx
+++ client/src/components/MypageList.tsx
@@ -1,8 +1,38 @@
+import styled from 'styled-components';
+
+const ListContent = styled.div`
+  height: 90vh;
+  width: 100%;
+
+  .list-line {
+    display: flex;
+    width: 100%;
+    height: 10%;
+    border-bottom: 1px solid var(--grayContentsBorder);
+    align-items: center;
+  }
+  .list-name {
+    font-size: 1rem;
+    margin-left: 2rem;
+  }
+
+  @media screen and (max-width: 414px) {
+    .list-line {
+      display: none;
+    }
+    .list-name {
+      display: none;
+    }
+  }
+`;
+
 const MypageList = () => {
   return (
-    <div>
-      <div>작성 글</div>
-    </div>
+    <ListContent>
+      <div className="list-line">
+        <span className="list-name">작성 글</span>
+      </div>
+    </ListContent>
   );
 };
 export default MypageList;
