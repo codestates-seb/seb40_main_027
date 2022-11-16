eeb619c8b5c674076cebde4b198ed291ab99c6e7
headring <myhakiki@gmail.com>
2022-11-16 14:53:10 +0900

    feat: useFrom 적용

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
-// import { useForm } from 'react-hook-form';
-import logo from '../image/logo.png';
+import { useForm } from 'react-hook-form';
+import logo from '../assets/image/logo.png';
 
 const LoginWrapp = styled.div`
+  height: 100vh;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   .login-inner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     img {
       width: 240px;
       height: 120px;
     }
     .form-wrap {
       margin-left: -1rem;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
     }
   }
 `;
@@ -25,14 +25,24 @@ const LoginWrapp = styled.div`
 // 이메일과 패스워드를 송신하면 유저아이디와 name을 준다
 
 const Login = () => {
-  // const { register } = useForm();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
   return (
     <LoginWrapp>
       <div className="login-inner">
         <img src={logo} alt="logo" />
         <div className="form-wrap">
-          <form>
-            <input type="email" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+              Email
+              <input {...register('email')} />
+              {/* <input type="submit" /> */}
+            </label>
+            <label>
+              Password
+              <input {...register('password')} />
+            </label>
+            <input type="submit" />
           </form>
         </div>
       </div>
