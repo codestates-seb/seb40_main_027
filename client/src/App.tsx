ab1274ecaa28b8f1e14b4848920eac88e15fb221
skyhanull <anky0425@naver.com>
2022-11-17 17:35:05 +0900

    rename:파일명 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Main from './pages/Main';
-import Mypage from './pages/Mypage';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/users/me" element={<Mypage />} />
+        <Route path="/users/me" element={<MyPage />} />
       </Routes>
       <Footer />
     </div>
