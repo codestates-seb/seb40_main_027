69b94f8d6651f8badf3083ab7ee22734d050c748
skyhanull <anky0425@naver.com>
2022-11-17 23:34:47 +0900

    design:style파일 생성

---
+++ client/src/components/MypageContent/MyPageSchedule.style.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const ScheduleContent = styled.div`
+  height: 90vh;
+  width: 100%;
+
+  .schedule-line {
+    display: flex;
+    width: 100%;
+    height: 10%;
+    border-bottom: 1px solid var(--grayContentsBorder);
+    align-items: center;
+  }
+  .schedule-name {
+    font-size: 1rem;
+    margin-left: 2rem;
+  }
+  @media screen and (max-width: 414px) {
+    .schedule-line {
+      display: none;
+    }
+    .schedule-name {
+      display: none;
+    }
+  }
+`;
+
+export const ScheduleName = styled.span`
+  font-size: 1rem;
+  margin-left: 2rem;
+  @media screen and (max-width: 414px) {
+    display: none;
+  }
+`;
