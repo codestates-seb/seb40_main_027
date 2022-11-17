d486a92ed7326976af4edb9fa5d4dc3b7c137c9f
pjnw1236 <pjnw1236@gmail.com>
2022-11-17 10:18:04 +0900

    Refactor: StudyCommentMapper remove commentsToCommentResponses

--- server/src/main/java/com/yes27/study_comment/mapper/StudyCommentMapper.java
+++ server/src/main/java/com/yes27/study_comment/mapper/StudyCommentMapper.java
@@ -2,7 +2,6 @@ package com.yes27.study_comment.mapper;
 
 import com.yes27.study_comment.dto.StudyCommentDto;
 import com.yes27.study_comment.entity.StudyComment;
-import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -10,5 +9,4 @@ public interface StudyCommentMapper {
     StudyComment commentPostToComment(StudyCommentDto.Post requestBody);
     StudyComment commentPatchToComment(StudyCommentDto.Patch requestBody);
     StudyCommentDto.Response commentToCommentResponse(StudyComment studyComment);
-    List<StudyCommentDto.Response> commentsToCommentResponses(List<StudyComment> studyComments);
 }
