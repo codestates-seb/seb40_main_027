e181426ade0052a083199303d9dee7bc71928208
skyhanull <anky0425@naver.com>
2022-11-17 23:34:21 +0900

    rename:파일위치변경

---
+++ client/src/components/MypageContent/MypageSchedule.tsx
@@ -0,0 +1,12 @@
+import * as S from './MyPageSchedule.style';
+
+const MypageSchedule = () => {
+  return (
+    <S.ScheduleContent>
+      <div className="schedule-line">
+        <S.ScheduleName>찜한 일정</S.ScheduleName>
+      </div>
+    </S.ScheduleContent>
+  );
+};
+export default MypageSchedule;
