b15df852a7a96919960a297ae392ac1c8f8a018b
skyhanull <anky0425@naver.com>
2022-11-18 10:10:16 +0900

    docs:interface수정

--- client/src/components/MypageContent/MypageList.tsx
+++ client/src/components/MypageContent/MypageList.tsx
@@ -40,18 +40,18 @@ const ListContent = styled.div`
   }
 `;
 //밑의 더미데이터는 임의로 모양만 잡기위해 넣어논 것입니다
-interface Dummy {
-  tag: string;
-  title: string;
-  content: string;
-  picture: string;
-  name: string;
-  time: number;
-  view: number;
-  heart: boolean;
-}
+// interface Dummy {
+//   tag: string;
+//   title: string;
+//   content: string;
+//   picture: string;
+//   name: string;
+//   time: number;
+//   view: number;
+//   heart: boolean;
+// }
 
-const DummyData: Dummy = {
+const DummyData = {
   tag: '1',
   title: '코드스테이츠 후기',
   content: '안녕하세요, 코드스테이츠 프론트엔드 부트캠프 40기 수료생입니다',
@@ -62,7 +62,7 @@ const DummyData: Dummy = {
   heart: false,
 };
 const MypageList = () => {
-  const DummyList: ReadonlyArray<Dummy> = [DummyData, DummyData];
+  const DummyList = [DummyData, DummyData];
   console.log(DummyList);
 
   return (
