c3fe8096adb56047ca602569eff4cf0576cf65b1
skyhanull <anky0425@naver.com>
2022-11-18 19:22:03 +0900

    docs:라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Footer from './components/Footer/Footer';
+import Test from './pages/Test';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/user/login" element={<Login />} />
+        <Route path="/user/test" element={<Test />} />
       </Routes>
       <Footer />
     </div>
