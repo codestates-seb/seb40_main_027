9d0a9e7c8fdbb9fb689cd425e3c538acc45e0f2a
skyhanull <anky0425@naver.com>
2022-11-18 19:23:16 +0900

    rename:tsx로 변경

---
+++ client/src/atoms/index.tsx
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+
+export const sidebarFloading = atom({
+  key: 'sidebarFloading',
+  default: false,
+});
+
+export const YesContent = atom({
+  key: 'YesContent',
+  default: 0,
+});
+
+export const NoContent = atom({
+  key: 'NoContent',
+  default: 0,
+});
