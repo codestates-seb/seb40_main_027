7ad9622f3929031e2d6daf1249e2d82634d86f44
headring <myhakiki@gmail.com>
2022-11-18 09:40:10 +0900

    style: div태그를 h2로 변경

--- client/src/components/Banner/index.style.tsx
+++ client/src/components/Banner/index.style.tsx
@@ -44,7 +44,7 @@ export const Character = styled.div`
   }
 `;
 
-export const Title = styled.div`
+export const Title = styled.h2`
   width: calc(100% / 3);
   display: flex;
   justify-content: center;
