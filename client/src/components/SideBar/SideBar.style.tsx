f1d9427c765e0cf8b5e339c3c09adb19846b1a04
skyhanull <anky0425@naver.com>
2022-11-19 23:39:37 +0900

    rename:sidebar 폴더로 이동

---
+++ client/src/components/SideBar/SideBar.style.tsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+export const SideBarPage = styled.div`
+  .sidebar-graypage {
+    visibility: hidden;
+    height: 0vh;
+    transition: all 0.3s;
+  }
+  @media screen and (max-width: 414px) {
+    .sidebar-graypage {
+      visibility: visible;
+
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.38);
+      animation: setMotion 1s normal;
+      @keyframes setMotion {
+        from {
+          width: 0%;
+        }
+
+        to {
+          width: 100%;
+        }
+      }
+    }
+
+    z-index: 2;
+
+    .sidebar-content {
+      width: 70%;
+      height: 100%;
+      position: absolute;
+      background-color: var(--whiteBackground);
+      transition: all 0.5s;
+      animation: setMotions 1s normal;
+      z-index: 2;
+    }
+    @keyframes setMotions {
+      from {
+        width: 0%;
+      }
+
+      to {
+        width: 70%;
+      }
+    }
+  }
+`;
+
+export const SideBarMenu = styled.div`
+  @media screen and (max-width: 414px) {
+    border: 1px solid var(--grayContentsBorder);
+    width: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10% 10% 10% 20%;
+    height: 4rem;
+  }
+`;
+
+export const HideSidebar = styled.div`
+  @media screen and (max-width: 414px) {
+    width: 1rem;
+    height: 1rem;
+    background-color: pink;
+    transition: all 0.25s;
+
+    position: absolute;
+    animation: setMotionHide 1s normal;
+  }
+  @keyframes setMotionHide {
+    from {
+      width: 100%;
+    }
+
+    to {
+      width: 0%;
+    }
+  }
+`;
---
+++ client/src/components/SideBar/SideBar.tsx
@@ -0,0 +1,35 @@
+import { useRecoilState } from 'recoil';
+import { sidebarFloading } from '../../atoms/index';
+import * as S from './SideBar.style';
+
+const SideBar = () => {
+  const [isContent, setIsContent] = useRecoilState(sidebarFloading);
+
+  const SelectMenu = ['적성검사', '로드맵', '학원일정', '수료후기', '스터디 모집', '멘토링'];
+
+  const HideHandler = () => {
+    setIsContent(!isContent);
+  };
+
+  return (
+    <div>
+      {isContent ? (
+        <S.SideBarPage onClick={HideHandler}>
+          <div className="sidebar-graypage">
+            <div className="sidebar-content">
+              <div>
+                {SelectMenu.map((el, idx) => (
+                  <S.SideBarMenu key={idx}>{el}</S.SideBarMenu>
+                ))}
+              </div>
+            </div>
+          </div>
+        </S.SideBarPage>
+      ) : (
+        <S.SideBarPage />
+      )}
+    </div>
+  );
+};
+
+export default SideBar;
