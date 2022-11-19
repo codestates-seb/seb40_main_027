368fdd82a981a62523e7101be2a015e74464bbba
skyhanull <anky0425@naver.com>
2022-11-19 23:25:47 +0900

    feat:적성검사 첫 페이지

--- client/src/components/StartForm.tsx
+++ client/src/components/StartForm.tsx
@@ -4,17 +4,18 @@ import styled from 'styled-components';
 
 const StartFormView = styled.div`
   width: 100%;
-  height: 100%;
-
   display: flex;
   justify-content: center;
   align-items: center;
+  @media screen and (max-width: 414px) {
+    margin: 0 1rem 0 1rem;
+  }
 `;
 
 const StartView = styled.div`
   display: flex;
   flex-direction: column;
-  height: 10rem;
+  height: 100%;
   justify-content: space-evenly;
   align-items: center;
   color: var(--whiteBackground);
@@ -22,11 +23,18 @@ const StartView = styled.div`
 `;
 
 const StartFormButton = styled.button`
+  border: none;
+  margin-top: 2rem;
   width: 7rem;
   height: 3rem;
   border-radius: 10px;
 
-  background-color: aquamarine;
+  background-color: aquamarine; //확정적인 색이 아니라 우선 임의로 사용
+  &:hover {
+    background-color: #a9dbbd; //마이페이지에 컬러지정해서 이곳에서는 우선 사용
+    color: var(--whiteBackground);
+    transition: all 1s;
+  }
 `;
 
 const StartForm = () => {
