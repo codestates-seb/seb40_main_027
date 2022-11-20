97085f01fc595fea1bbca205f8797d2684d7b723
headring <myhakiki@gmail.com>
2022-11-20 19:51:26 +0900

    feat: props받아서 텍스트 대체 기능 추가

--- client/src/components/Button/RegisterButton.tsx
+++ client/src/components/Button/RegisterButton.tsx
@@ -15,6 +15,10 @@ const StyledButton = styled(StyledBackgroundButton)`
   }
 `;
 
-export const RegisterButton = () => {
-  return <StyledButton>회원가입</StyledButton>;
+interface PropType {
+  text?: string;
+}
+
+export const RegisterButton = ({ text }: PropType) => {
+  return <StyledButton>{text ? text : '회원가입'}</StyledButton>;
 };
