d19621aca3db63f9061b1736f7dc2a7764f113ea
headring <myhakiki@gmail.com>
2022-11-20 20:07:58 +0900

    rename: index를 SearchBar.tsx로 변경

---
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,48 @@
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+
+const SearchWrapper = styled.form`
+  width: 200px; // 임시 조정(추후 페이지에 따라 조정 또는 리펙토링 필요)
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid var(--grayContentsBorder);
+  border-radius: 3px;
+  input {
+    padding-left: 1rem;
+    width: 100%;
+    padding-top: 0.4rem;
+    padding-bottom: 0.4rem;
+    /* height: 100%;  */
+    border: none;
+    outline: none;
+    font-size: 80%;
+  }
+`;
+
+const SearchBar = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  return (
+    <SearchWrapper onSubmit={handleSubmit(onSubmit)}>
+      <Icon icon="ant-design:search-outlined" />
+      <input type="text" placeholder="검색" {...register('username')} />
+    </SearchWrapper>
+  );
+};
+
+// const SearchBar = () => {
+//   const [searchText, setSearchText] = useState('');
+//   const handleChange = (e: any) => setSearchText(e.target.value);
+//   // api 연동 시 검색이 가능한 onSubmit 필요
+
+//   return (
+//     <SearchWrapper>
+//       <Icon icon="ant-design:search-outlined" />
+//       <input type="text" className="search-bar" placeholder="검색" value={searchText} onChange={handleChange}></input>
+//     </SearchWrapper>
+//   );
+// };
+export default SearchBar;
