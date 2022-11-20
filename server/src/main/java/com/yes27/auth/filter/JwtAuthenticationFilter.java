3adae4be6e2274ee5ec4c0cb05d4737f38ced6c3
pjnw1236 <pjnw1236@gmail.com>
2022-11-21 03:25:58 +0900

    feat: 로그인 인증 요청 처리하는 엔트리포인트 JwtAuthenticationFilter 생성

---
+++ server/src/main/java/com/yes27/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,85 @@
+package com.yes27.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yes27.auth.dto.LoginDto;
+import com.yes27.auth.jwt.JwtTokenizer;
+import com.yes27.member.entity.Member;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// 클라이언트의 로그인 인증 요청 처리하는 엔트리포인트(Entrypoint)
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenizer jwtTokenizer;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenizer jwtTokenizer) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    // (3)
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+        ObjectMapper objectMapper = new ObjectMapper();    // (3-1)
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class); // (3-2)
+
+        // (3-3)
+        UsernamePasswordAuthenticationToken authenticationToken =
+            new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        return authenticationManager.authenticate(authenticationToken);  // (3-4)
+    }
+
+    // (4)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain chain,
+        Authentication authResult) {
+        Member member = (Member) authResult.getPrincipal();  // (4-1)
+
+        String accessToken = delegateAccessToken(member);   // Access Token 생성
+        String refreshToken = delegateRefreshToken(member); // Refresh Token 생성
+
+        response.setHeader("Authorization", "Bearer " + accessToken);  // (4-4)
+        response.setHeader("Refresh", refreshToken);                   // (4-5)
+    }
+
+    // Access Token 생성 로직
+    private String delegateAccessToken(Member member) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", member.getEmail());
+        claims.put("roles", member.getRoles());
+
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    // Refresh Token 생성 로직
+    private String delegateRefreshToken(Member member) {
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+}
