2434262a23d4e0b9692205108aecb14ba80c06ec
pjnw1236 <pjnw1236@gmail.com>
2022-11-21 02:01:05 +0900

    fix: jwt 적용을 위한 SecurityConfig 수정

--- server/src/main/java/com/yes27/config/SecurityConfig.java
+++ server/src/main/java/com/yes27/config/SecurityConfig.java
@@ -1,20 +1,49 @@
 package com.yes27.config;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-            .anyRequest()
-            .permitAll();
-
+            .headers().frameOptions().sameOrigin() // (1)
+            .and()
+            .csrf().disable()        // (2)
+            .cors(withDefaults())    // (3)
+            .formLogin().disable()   // (4)
+            .httpBasic().disable()   // (5)
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll()                // (6)
+            );
         return http.build();
+    }
+
+    // (7)
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));   // (8-1)
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE"));  // (8-2)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();   // (8-3)
+        source.registerCorsConfiguration("/**", configuration);      // (8-4)    
+        return source;
     }
 }
