31e9c7f714d649a5703633d712ac196898a18a25
skyhanull <anky0425@naver.com>
2022-11-21 12:57:36 +0900

    feat:startform스타일 구현

---
+++ client/src/components/TestForm/StartForm.style.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const StartFormView = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 414px) {
+    margin: 0 20% 0 0;
+  }
+`;
+
+export const StartView = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-evenly;
+  align-items: center;
+  color: var(--whiteBackground);
+  font-size: 1.5rem;
+  animation: shake 0.5s infinite;
+
+  @media screen and (max-width: 414px) {
+    font-size: 1rem;
+    justify-content: center;
+    margin: 0px 10% 0 10%;
+    animation: fadein 7s ease-in-out;
+    @keyframes fadein {
+      0% {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      100% {
+        opacity: 1;
+        transform: none;
+      }
+    }
+  }
+`;
+
+export const StartFormButton = styled.button`
+  border: none;
+  margin-top: 2rem;
+  width: 7rem;
+  height: 3rem;
+  border-radius: 10px;
+  font-weight: bold;
+
+  background-color: aquamarine; //확정적인 색이 아니라 우선 임의로 사용
+  &:hover {
+    background-color: var(--greenSub); //마이페이지에 컬러지정해서 이곳에서는 우선 사용
+    color: var(--whiteBackground);
+    transition: all 1s;
+  }
+`;
