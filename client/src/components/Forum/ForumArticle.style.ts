45d2ed5652844a762259b6c79a1a41da0d49705b
Jongwon Park <jwp7073@gmail.com>
2022-11-21 16:42:40 +0900

    feat: 게시판 상세 list view 구현

---
+++ client/src/components/Forum/ForumArticle.style.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Article = styled.article`
+  padding: calc(20 / 16 * 1rem);
+  border-bottom: 1px solid var(--grayContentsBorder);
+
+  &:first-child {
+    border-top: 1px solid var(--grayContentsBorder);
+  }
+
+  & > * > *:not(:last-child) {
+    margin-bottom: calc(10 / 16 * 1rem);
+  }
+
+  @media screen and (max-width: 414px) {
+    padding: calc(10 / 16 * 1rem);
+    width: calc(100% - (20 / 16 * 1rem));
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+export const TagsContainer = styled.div`
+  & > button:not(:last-child) {
+    margin-right: calc(10 / 16 * 1rem);
+  }
+`;
+
+export const Title = styled.h3`
+  font-weight: 700;
+  font-size: calc(18 / 16 * 1rem);
+`;
+
+export const Content = styled.div`
+  font-size: calc(14 / 16 * 1rem);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  @media screen and (max-width: 414px) {
+    display: none;
+  }
+`;
---
+++ client/src/components/Forum/ForumArticles.style.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  width: calc(100% - (197 / 16 * 1rem));
+
+  & > *:not(:last-child) {
+    margin-bottom: calc(20 / 16 * 1rem);
+  }
+
+  @media screen and (max-width: 414px) {
+    width: calc(100% - (10 / 16 * 1rem));
+
+    & > *:not(:last-child) {
+      margin-bottom: calc(10 / 16 * 1rem);
+    }
+  }
+`;
+
+export const ContentHeader = styled.section`
+  display: flex;
+  justify-content: space-between;
+  height: calc(36 / 16 * 1rem);
+
+  & > *:not(:last-child) {
+    margin-bottom: calc(20 / 16 * 1rem);
+  }
+
+  @media screen and (max-width: 414px) {
+    display: flex;
+    flex-direction: column-reverse;
+    width: calc(100% - (20 / 16 * 1rem));
+    height: calc(70 / 16 * 1rem);
+
+    & > *:not(:last-child) {
+      margin-bottom: 0;
+    }
+
+    & > div:last-child {
+      margin-bottom: calc(10 / 16 * 1rem);
+
+      & > form {
+        width: calc(100% - (176 / 16 * 1rem));
+      }
+    }
+  }
+`;
+
+export const TagsContainer = styled.div`
+  display: flex;
+
+  & > *:not(:last-child) {
+    margin-right: calc(10 / 16 * 1rem);
+  }
+`;
+
+export const OtherContainer = styled.div`
+  display: flex;
+
+  & > *:not(:last-child) {
+    margin-right: calc(10 / 16 * 1rem);
+  }
+`;
