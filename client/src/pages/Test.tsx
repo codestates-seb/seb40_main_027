aa07b178e23cd68307761ca710ec2b67e5f1e5dd
skyhanull <anky0425@naver.com>
2022-11-21 12:59:11 +0900

    feat:적성검사 페이지 구현

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,46 +1,18 @@
 import PageHeader from '../components/Header/PageHeader';
-import styled from 'styled-components';
-
-import StartForm from '../components/StartForm';
-const TestContent = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const FormContent = styled.div`
-  display: flexzz;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--greenMain);
-  height: 80%;
-  width: 70%;
-  @media screen and (max-width: 414px) {
-    height: 60vh;
-    width: 100vw;
-  }
-`;
-
-const StyleFormCode = styled.div`
-  border: 6px double white;
-  width: 90%;
-  height: 90%;
-  display: flex;
-  align-items: center;
-`;
+import * as S from './Test.style';
+import StartForm from '../components/TestForm/StartForm';
 
 const Test = () => {
   return (
     <div>
       <PageHeader />
-      <TestContent>
-        <FormContent>
-          <StyleFormCode>
+      <S.TestContent>
+        <S.FormContent>
+          <S.StyleFormCode>
             <StartForm />
-          </StyleFormCode>
-        </FormContent>
-      </TestContent>
+          </S.StyleFormCode>
+        </S.FormContent>
+      </S.TestContent>
     </div>
   );
 };
