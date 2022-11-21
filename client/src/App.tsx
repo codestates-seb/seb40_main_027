2f1bb99c723fee3f677220ebdb7edfbfc601e3e2
Jongwon Park <jwp7073@gmail.com>
2022-11-21 18:08:40 +0900

    feat: 게시판 route 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Login from './pages/Login';
 import Footer from './components/Footer/Footer';
 import Main from './pages/Main';
 import MyPage from './pages/MyPage';
+import Forum from './pages/Forum';
 
 function App() {
   return (
@@ -11,6 +12,15 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/user/login" element={<Login />} />
         <Route path="/users/me" element={<MyPage />} />
+        <Route path="/postscript" element={<Forum />} />
+        <Route path="/postscript/:id" element={<Forum />} />
+        <Route path="/postscript/write" element={<Forum />} />
+        <Route path="/study" element={<Forum />} />
+        <Route path="/study/:id" element={<Forum />} />
+        <Route path="/study/write" element={<Forum />} />
+        <Route path="/mentoring" element={<Forum />} />
+        <Route path="/mentoring/:id" element={<Forum />} />
+        <Route path="/mentoring/write" element={<Forum />} />
       </Routes>
 
       <Footer />
