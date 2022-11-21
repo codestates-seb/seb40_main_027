70358c8afd61e34603c9027638dd5f3163dde016
skyhanull <anky0425@naver.com>
2022-11-21 12:58:40 +0900

    feat:경고문페이지 구현

---
+++ client/src/components/TestForm/WarningWindow.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import TypeForm from './TypeForm';
+import * as S from './WarningWindow.style';
+
+const WarningWindow = () => {
+  const [isWarning, setIsWarning] = useState<boolean>(false);
+
+  const WaringHandler = () => {
+    setIsWarning(!isWarning);
+  };
+  return (
+    <S.WarningLargeContent>
+      {isWarning ? (
+        <TypeForm />
+      ) : (
+        <div>
+          <S.WarnTitle>경고!</S.WarnTitle>
+          <S.WarnContent>이 테스트는 정확한 테스트가 아니므로 주의해주십시오.</S.WarnContent>
+          <S.WaringButtonContent>
+            <S.WarningButton onClick={WaringHandler}>Next</S.WarningButton>
+          </S.WaringButtonContent>
+        </div>
+      )}
+    </S.WarningLargeContent>
+  );
+};
+export default WarningWindow;
