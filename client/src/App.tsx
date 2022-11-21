2f1082528a0ac3c88a17d32ad6bc749d86bd92f8
skyhanull <anky0425@naver.com>
2022-11-21 20:35:10 +0900

    docs: url수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/user/login" element={<Login />} />
-        <Route path="/user/test" element={<Test />} />
+        <Route path="/test" element={<Test />} />
         <Route path="/users/me" element={<MyPage />} />
       </Routes>
 
