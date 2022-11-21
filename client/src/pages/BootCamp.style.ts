5a9b7043ef493ee400a2e70deb6b6a0f5a4a3dc6
headring <myhakiki@gmail.com>
2022-11-22 01:51:56 +0900

    refactor: 분리한 컴포넌트 적용 + 모바일 적용

--- client/src/pages/BootCamp.style.ts
+++ client/src/pages/BootCamp.style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import theme from '../styles/theme';
 
 export const PageWrap = styled.div`
   width: 100vw;
@@ -6,11 +7,16 @@ export const PageWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media ${theme.mobile} {
+    width: 100vw;
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const MiddleSection = styled.section`
   width: 60vw;
-  min-width: 960px;
   display: flex;
   justify-content: flex-end;
   margin-bottom: 3%;
@@ -19,39 +25,7 @@ export const MiddleSection = styled.section`
     justify-content: flex-end;
     width: calc(100% / 3);
   }
-`;
-
-export const TableWrap = styled.table`
-  width: 60vw;
-  min-width: 960px;
-  font-size: 20px;
-  thead {
-    font-weight: bold;
-    background-color: var(--greenTableHeader);
-    width: calc(100% / 6);
-    th {
-      border: 1px solid var(--grayTableBorder);
-      :before {
-        border: 2px solid red;
-        border-radius: 5px;
-      }
-      // 우측과 좌측 상단 border값에 radius주기가 어려워서 pass
-      /* :first-child {
-        border-radius: 10px 0 0 0;
-      }
-      :last-child {
-        border-radius: 0 10px 0 0;
-      } */
-    }
-  }
-  tbody {
-    tr {
-      border: 1px solid var(--grayTableBorder);
-      td {
-        padding: calc(100vh / 40) 0;
-        border: 1px solid var(--grayTableBorder);
-        text-align: center;
-      }
-    }
+  @media ${theme.mobile} {
+    display: none;
   }
 `;
--- client/src/pages/BootCamp.tsx
+++ client/src/pages/BootCamp.tsx
@@ -2,69 +2,10 @@ import Banner from '../components/Banner';
 import { FilterButton } from '../components/Button';
 import SearchBar from '../components/SearchBar';
 import * as S from './BootCamp.style';
-import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import PageHeader from '../components/Header/PageHeader';
-
-const data = [
-  {
-    name: '코드 스테이츠',
-    date: '10/11-14',
-    dueDate: '10/14',
-    filed: '프론트엔드',
-    cost: '무료(국비)',
-    onOff: '온라인',
-  },
-  {
-    name: '코드 스테이츠',
-    date: '10/11-14',
-    dueDate: '10/14',
-    filed: '프론트엔드',
-    cost: '무료(국비)',
-    onOff: '온라인',
-  },
-  {
-    name: '코드 스테이츠',
-    date: '10/11-14',
-    dueDate: '10/14',
-    filed: '프론트엔드',
-    cost: '무료(국비)',
-    onOff: '온라인',
-  },
-  {
-    name: '코드 스테이츠',
-    date: '10/11-14',
-    dueDate: '10/14',
-    filed: '프론트엔드',
-    cost: '무료(국비)',
-    onOff: '온라인',
-  },
-];
-
-interface BootData {
-  name: string;
-  date: string;
-  dueDate: string;
-  filed: string;
-  cost: string;
-  onOff: string;
-}
+import { MobileTable, Table } from '../components/Table/Table';
 
 const BootCamp = () => {
-  const columnHelper = createColumnHelper<BootData>();
-  const columns = [
-    columnHelper.accessor('name', { header: '이름', maxSize: 10 }),
-    columnHelper.accessor('date', { header: '등록일', maxSize: 10 }),
-    columnHelper.accessor('dueDate', { header: '마감일', maxSize: 50 }),
-    columnHelper.accessor('filed', { header: '과정', maxSize: 50 }),
-    columnHelper.accessor('cost', { header: '총 비용', maxSize: 50 }),
-    columnHelper.accessor('onOff', { header: '온/오프라인', maxSize: 50 }),
-  ];
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-
   return (
     <>
       <PageHeader />
@@ -79,28 +20,8 @@ const BootCamp = () => {
           </div>
         </S.MiddleSection>
         <section>
-          <S.TableWrap>
-            <thead>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <tr key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => (
-                    <th key={header.id}>
-                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                    </th>
-                  ))}
-                </tr>
-              ))}
-            </thead>
-            <tbody>
-              {table.getRowModel().rows.map((row) => (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map((cell) => (
-                    <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </S.TableWrap>
+          <Table />
+          <MobileTable />
         </section>
       </S.PageWrap>
     </>
