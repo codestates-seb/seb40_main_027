7a81271b8768369c6277ad29a10712d3fa83cdf6
headring <myhakiki@gmail.com>
2022-11-23 02:55:02 +0900

    style: 부트캠프 데스크탑 및 모바일 완성

--- client/src/pages/BootCampSepcific.style.ts
+++ client/src/pages/BootCampSepcific.style.ts
@@ -19,8 +19,8 @@ export const MiddleSection = styled.section`
   width: 60vw;
   display: flex;
   align-items: center;
-  justify-content: space-around;
-  margin-bottom: 3%;
+  justify-content: space-evenly;
+  margin-bottom: 0.4%;
   div {
     margin: calc(100% / 30) 0;
   }
@@ -43,8 +43,71 @@ export const grayBtn = styled.button`
 `;
 
 export const Detail = styled.div`
-  width: 100%;
+  width: 60vw;
+  height: calc(100vh - 20vh);
+  border: 3px solid var(--greenBootSpecificBorder);
+  border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
+  @media ${theme.mobile} {
+    flex-direction: column;
+  }
+  > div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    :first-child {
+      border-right: 2px solid var(--greenBootSpecificBorder);
+      @media ${theme.mobile} {
+        border: none;
+      }
+    }
+    > div {
+      display: flex;
+      width: 100%;
+      height: calc(100% / 3);
+      justify-content: center;
+      align-items: center;
+      > div {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+        @media ${theme.mobile} {
+          font-size: 4vw;
+        }
+        :first-child {
+          margin-left: calc(100vh / 50);
+        }
+        :last-child {
+          margin-right: calc(100vh / 60);
+        }
+      }
+    }
+  }
+  @media ${theme.mobile} {
+    width: 90vw;
+  }
+`;
+
+export const RowHeader = styled.div`
+  width: 65%;
+  height: 20%;
+  border: 1px solid var(--greenMain);
+  font-size: 1.12vw;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media ${theme.mobile} {
+    width: 85%;
+    height: 40%;
+    font-size: 4vw;
+  }
 `;
--- client/src/pages/BootCampSpecific.tsx
+++ client/src/pages/BootCampSpecific.tsx
@@ -2,17 +2,35 @@ import Banner from '../components/Banner';
 import { Icon } from '@iconify/react';
 import * as S from './BootCampSepcific.style';
 
+// 데이터 확정이 아직 나지 않아서 어떤 데이터 렌더링 될지 한번더 체크 필요
+
+// 통신으로 받아온 데이터의 key값에 해당하는 한글 호출하여 렌더링
+const dict = {
+  title: '기관정보',
+  process: '과정',
+  On_Off: '온/오프라인',
+  total_cost: '총 비용',
+  begin_register_date: '접수일',
+  place: null,
+  final_register_date: '접수마갑일',
+  start_date: '훈련기간',
+  end_date: '훈련기간',
+  duration: '공부기간',
+};
+
 const data = {
   name: '코드 스테이츠',
   date: '10/11-14',
-  onOff: '온라인',
+  On_Off: '온라인',
   duration: '6개월',
-  cost: '무료(국비)',
+  total_cost: '무료(국비)',
   timeTable: '시간표보기',
   satisfaction: '3',
   time: '09:00 ~ 18:00',
   incentives: '월 최대 316,0000d원',
 };
+
+const keysObj: Array<string> = Object.keys(data);
 
 const BootCampSpecific = () => {
   return (
@@ -31,26 +49,55 @@ const BootCampSpecific = () => {
         </S.MiddleSection>
         <section>
           <S.Detail>
+            {/* <div>
+              {keysObj.map((el, idx) =>
+                idx <= Math.ceil(keysObj.length / 2) ? (
+                  <div key={idx}>
+                    <div>
+                      <S.RowHeader>{data[el]}</S.RowHeader>
+                    </div>
+                  </div>
+                ) : null
+              )}
+            </div> */}
             <div>
               <div>
-                <span>모집기간</span> <span>{data.date}</span>
+                <div>
+                  <S.RowHeader>모집기간</S.RowHeader>
+                </div>
+                <div>{data.date}</div>
               </div>
               <div>
-                <div>공부기간</div>
+                <div>
+                  <S.RowHeader>공부기간</S.RowHeader>
+                </div>
+                <div>{data.duration}</div>
               </div>
               <div>
-                <div>온/오프라인</div>
+                <div>
+                  <S.RowHeader>온/오프라인</S.RowHeader>
+                </div>
+                <div>{data.On_Off}</div>
               </div>
             </div>
             <div>
               <div>
-                <div>시간표</div>
+                <div>
+                  <S.RowHeader>시간표</S.RowHeader>
+                </div>
+                <div>{data.time}</div>
               </div>
               <div>
-                <div>수강색 평균 만족도</div>
+                <div>
+                  <S.RowHeader>수강색 평균 만족도</S.RowHeader>
+                </div>
+                <div>{data.satisfaction}</div>
               </div>
               <div>
-                <div>시간</div>
+                <div>
+                  <S.RowHeader>시간</S.RowHeader>
+                </div>
+                <div>{data.time}</div>
               </div>
             </div>
           </S.Detail>
