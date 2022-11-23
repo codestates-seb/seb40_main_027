de6c548d449b6ba6463da8b2a359b3ed538acacf
skyhanull <anky0425@naver.com>
2022-11-23 10:32:51 +0900

    design:css변경

--- client/src/components/MyPageContent/UserProfile.tsx
+++ client/src/components/MyPageContent/UserProfile.tsx
@@ -4,28 +4,15 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { useState } from 'react';
 import axios from 'axios';
 
-const ProfileUpdateButton = styled.button`
-  border: none;
-  background-color: var(--whiteBackground);
-`;
-
 const MyProfileView = styled.div`
-  width: 14vw;
+  //전체 길이
+  width: 16vw;
   border-right: 1px solid var(--grayContentsBorder);
   height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: flex-start;
   flex-direction: column;
 
-  .border-line {
-    border: 1px solid gray;
-    width: 100%;
-    height: calc(400 / 14 * 1rem);
-  }
-
   .user-info {
-    height: 100rem;
     width: 100%;
     display: flex;
     justify-content: center;
@@ -34,30 +21,55 @@ const MyProfileView = styled.div`
   @media screen and (max-width: 414px) {
     border: none;
     height: 100%;
+    display: flex;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+
+    .user-info {
+      width: 100vw;
+      height: 20vh;
+      margin: 0;
+    }
   }
 `;
 
-const GrayProfileView = styled.div`
-  width: 100%;
-  height: 100%;
+const ProfileUpdateButton = styled.button`
+  border: none;
+  background-color: var(--whiteBackground);
+  display: flex;
+  justify-content: flex-start;
 `;
+
 const UserProfileUpdateBody = styled.div`
-  width: 90%;
-  height: 14vh;
+  //프로필 전체크기
+  width: 100%;
+  height: 45vh;
   display: flex;
   justify-content: center;
   align-items: flex-start;
   flex-direction: column;
-  margin: 0 0 0 1rem;
+  background-color: aquamarine;
+  border: 1px solid gray;
+
+  @media screen and (max-width: 414px) {
+    justify-content: center;
+    height: 20vh;
+    flex-direction: row;
+  }
 `;
 
-const PictureProfile = styled.div`
-  height: 13rem;
+const PictureProfile = styled.span`
+  height: 20vh;
   width: 70%;
-  border-radius: 50%;
 
+  display: flex;
   border: 1px solid black;
-  margin: 5% 5% 5% 15%;
+  margin: 0% 0% 1% 10%;
+  @media screen and (max-width: 414px) {
+    height: 100%;
+    margin: 0;
+  }
 `;
 
 const FormInputProFile = styled.form`
@@ -67,6 +79,15 @@ const FormInputProFile = styled.form`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  margin-top: 2rem;
+  height: 100%;
+  @media screen and (max-width: 414px) {
+    height: 100%;
+    width: 100vw;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0;
+  }
 `;
 const InputProfileForm = styled.input`
   width: 90%;
@@ -74,14 +95,21 @@ const InputProfileForm = styled.input`
   height: 1.5rem;
   margin-bottom: 1rem;
   border-radius: 10px;
+  @media screen and (max-width: 414px) {
+    margin: 0;
+    height: 1rem;
+    width: 80%;
+  }
 `;
 
-const UserInfoFormEmail = styled.div`
+const UserInfoFormEmail = styled.span`
   background-color: var(--grayHeaderBorder);
   display: inline;
   width: 100%;
   height: 2rem;
   border-radius: 10px;
+  @media screen and (max-width: 414px) {
+  }
 `;
 
 interface IFormInput {
@@ -117,36 +145,33 @@ const UserProfile = () => {
 
   return (
     <MyProfileView>
-      <GrayProfileView>
-        <ProfileUpdateButton onClick={onClickUpdate}>
-          <Icon icon="ph:gear-six-duotone" width="25" height="25" />
-        </ProfileUpdateButton>
-        <div className="border-line">
-          <PictureProfile></PictureProfile>
-          {updateProfile ? (
-            <UserProfileUpdateBody>
-              <FormInputProFile onSubmit={handleSubmit(MyProfileSubmit)}>
-                <label htmlFor="email">email</label>
-                <InputProfileForm {...register('email')} />
-                <label htmlFor="nickname">nickname</label>
-                <InputProfileForm {...register('nickname')} />
-                <label htmlFor="password">비밀번호</label>
-                <InputProfileForm {...register('password')} />
+      <ProfileUpdateButton onClick={onClickUpdate}>
+        <Icon icon="ph:gear-six-duotone" width="25" height="25" />
+      </ProfileUpdateButton>
 
-                <input type="submit" />
-              </FormInputProFile>
-            </UserProfileUpdateBody>
-          ) : (
-            <UserProfileUpdateBody>
-              <div className="user-info">
-                nickname
-                <UserInfoFormEmail></UserInfoFormEmail>
-                email<UserInfoFormEmail></UserInfoFormEmail>
-              </div>
-            </UserProfileUpdateBody>
-          )}
-        </div>
-      </GrayProfileView>
+      {updateProfile ? (
+        <UserProfileUpdateBody>
+          <FormInputProFile onSubmit={handleSubmit(MyProfileSubmit)}>
+            <label htmlFor="email">email</label>
+            <InputProfileForm {...register('email')} />
+            <label htmlFor="nickname">nickname</label>
+            <InputProfileForm {...register('nickname')} />
+            <label htmlFor="password">비밀번호</label>
+            <InputProfileForm {...register('password')} />
+
+            <input type="submit" />
+          </FormInputProFile>
+        </UserProfileUpdateBody>
+      ) : (
+        <UserProfileUpdateBody>
+          <PictureProfile></PictureProfile>
+          <div className="user-info">
+            nickname
+            <UserInfoFormEmail></UserInfoFormEmail>
+            email<UserInfoFormEmail></UserInfoFormEmail>
+          </div>
+        </UserProfileUpdateBody>
+      )}
     </MyProfileView>
   );
 };
