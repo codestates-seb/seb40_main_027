e23dbbd7f79f048a124543e1d0e753ea8402a179
pjnw1236 <pjnw1236@gmail.com>
2022-11-24 14:44:37 +0900

    feat: StudyRepository StudyVoteRepository

--- server/src/main/java/com/yes27/study/repository/StudyVoteRepository.java
+++ server/src/main/java/com/yes27/study/repository/StudyVoteRepository.java
@@ -1,10 +1,11 @@
 package com.yes27.study.repository;
 
 import com.yes27.member.entity.Member;
+import com.yes27.study.entity.Study;
 import com.yes27.study.entity.StudyVote;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyVoteRepository extends JpaRepository<StudyVote, Long> {
-    Optional<StudyVote> findByVoteAndMember(Member member, StudyVote studyVote);
+    Optional<StudyVote> findByMemberAndStudy(Member member, Optional<Study> study);
 }
