d47a3e78d038e1040974b9188d02b6108102084e
skyhanull <anky0425@naver.com>
2022-11-24 13:14:48 +0900

    docs:dummy양 수정

--- client/src/components/MyPageContent/MyPageList.tsx
+++ client/src/components/MyPageContent/MyPageList.tsx
@@ -99,54 +99,7 @@ const dummyDatalist = [
     createdAt: '2022-11-20T16:35:41.87709',
     updatedAt: '2022-11-20T16:35:41.87709',
   },
-  {
-    postscriptId: 1,
-    postscriptTitle: '코드스테이츠 프론트엔드 후기',
-    postscriptContent:
-      '<p>안녕하세요. 코드스테이츠 프론트엔드 부트캠프 후기입니다. 저는 수료 후 네카라쿠배 중 1곳에 취업했습니다.</p><h3>부트캠프 활용 꿀팁</h3><p>동기들과의 스터디에 적극 참여하세요.<br>매일 알고리즘 문제 1개 이상 풀기</p>',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    view: 32,
-    like: 2,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-20T16:35:41.87709',
-    updatedAt: '2022-11-20T16:35:41.87709',
-  },
-  {
-    postscriptId: 1,
-    postscriptTitle: '코드스테이츠 프론트엔드 후기',
-    postscriptContent:
-      '<p>안녕하세요. 코드스테이츠 프론트엔드 부트캠프 후기입니다. 저는 수료 후 네카라쿠배 중 1곳에 취업했습니다.</p><h3>부트캠프 활용 꿀팁</h3><p>동기들과의 스터디에 적극 참여하세요.<br>매일 알고리즘 문제 1개 이상 풀기</p>',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    view: 32,
-    like: 2,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-20T16:35:41.87709',
-    updatedAt: '2022-11-20T16:35:41.87709',
-  },
-  {
-    postscriptId: 1,
-    postscriptTitle: '코드스테이츠 프론트엔드 후기',
-    postscriptContent:
-      '<p>안녕하세요. 코드스테이츠 프론트엔드 부트캠프 후기입니다. 저는 수료 후 네카라쿠배 중 1곳에 취업했습니다.</p><h3>부트캠프 활용 꿀팁</h3><p>동기들과의 스터디에 적극 참여하세요.<br>매일 알고리즘 문제 1개 이상 풀기</p>',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    view: 32,
-    like: 2,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-20T16:35:41.87709',
-    updatedAt: '2022-11-20T16:35:41.87709',
-  },
+
   {
     postscriptId: 1,
     postscriptTitle: '코드스테이츠 프론트엔드 후기',
@@ -188,8 +141,8 @@ const MyPageList = () => {
         <span className="list-name">작성 글</span>
       </div>
       <div className="content-list">
-        {dummyDatalist.map((list) => (
-          <MyPageLists key={list.postscriptId} list={list} />
+        {dummyDatalist.map((list, idx) => (
+          <MyPageLists key={idx} list={list} />
         ))}
       </div>
     </ListContent>
