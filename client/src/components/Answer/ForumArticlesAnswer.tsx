4b60961ccc468be0074fbe299d6472955742bf8f
skyhanull <anky0425@naver.com>
2022-11-25 15:49:09 +0900

    docs:api통신

--- client/src/components/Answer/ForumArticlesAnswer.tsx
+++ client/src/components/Answer/ForumArticlesAnswer.tsx
@@ -1,13 +1,22 @@
 import { useRef, useState, useMemo } from 'react';
 import ReactQuill from 'react-quill';
-// import styled from 'styled-components';
-// import axios from 'axios';
+import styled from 'styled-components';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router';
 
 import 'react-quill/dist/quill.snow.css';
 
 const ForumArticlesAnswer = () => {
-  const QuillRef = useRef<ReactQuill>();
-  const [contents, setContents] = useState('');
+  // const QuillRef = useRef<ReactQuill>();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  console.log(id);
+  const [answerContents, setAnswerContents] = useState('');
+
+  const ContentsHandler = (event: any) => {
+    setAnswerContents(event);
+    console.log(answerContents);
+  };
 
   const modules = useMemo(
     () => ({
@@ -18,28 +27,43 @@ const ForumArticlesAnswer = () => {
           [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }, { align: [] }],
           ['image', 'video'],
         ],
-        handlers: {
-          // image: imageHandler,
-        },
+        handlers: {},
       },
     }),
     []
   );
 
+  const SummitAnswerBtn = () => {
+    axios.defaults.withCredentials = true;
+    axios({
+      method: 'post',
+      url: `/postscript/${id}/comment`,
+      data: { answerContents },
+      // headers: {
+      //   Authorization:
+      //     'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoibGFsYUBnbWFpbC5jb20iLCJzdWIiOiJsYWxhQGdtYWlsLmNvbSIsImlhdCI6MTY2OTM1NTAxOSwiZXhwIjoxNjY5Mzk4MjE5fQ.MN3n0GS62o6xcOsT2GVrsYhdAhFltA-dUHjmTgaZlBemr_xEv-CGJQI6gj0edUdAjF5PEF82GhkTl_eDlPyXEQ',
+      // },
+    })
+      .then((res) => navigate('/postscript/${id}/comment'))
+      .catch(() => console.log('err'));
+  };
+
   return (
     <>
-      <ReactQuill
+      {/* <ReactQuill
         ref={(element) => {
           if (element !== null) {
             QuillRef.current = element;
           }
         }}
-        value={contents}
-        onChange={setContents}
+        value={answerContents || ''}
+        onChange={ContentsHandler}
         modules={modules}
         theme="snow"
         placeholder="내용을 입력해주세요."
-      />
+      /> */}
+      <ReactQuill theme="snow" value={answerContents} onChange={ContentsHandler} />;
+      <button onClick={SummitAnswerBtn}>추가</button>
     </>
   );
 };
