a6cc8f154dac68d7c08af82b7e4d1699cf5c78da
didwndbs <didwndbs@naver.com>
2022-11-25 10:08:58 +0900

    feat: Mentoring게시판 좋아요 기능, 정렬 기능 구현

--- server/src/main/java/com/yes27/mentoring/repository/MentorRepository.java
+++ server/src/main/java/com/yes27/mentoring/repository/MentorRepository.java
@@ -1,8 +1,16 @@
 package com.yes27.mentoring.repository;
 
+import com.yes27.member.entity.Member;
 import com.yes27.mentoring.entity.Mentor;
+import com.yes27.mentoringLike.entity.MentoringVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MentorRepository extends JpaRepository<Mentor, Long> {
+
+    Optional<Mentor> findByMentoringIdAndMember(Long mentoringId, Member member);
+
 
 }
