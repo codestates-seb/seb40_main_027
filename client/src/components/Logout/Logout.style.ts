c421c6dc81cf4d23ff36bb4c275feefa5f87232d
skyhanull <anky0425@naver.com>
2022-11-25 13:46:56 +0900

    feat:logout css 생성

---
+++ client/src/components/Logout/Logout.style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { StyledBackgroundButton } from '../Button/BackgroundButton';
+
+export const LogoutButton = styled(StyledBackgroundButton)`
+  width: 84px;
+  height: 36px;
+  border-radius: 9px;
+  font-size: 20px;
+  @media screen and (max-width: 414px) {
+    width: 70px;
+    height: 30px;
+    border-radius: 4.5px;
+    font-size: 10px;
+  }
+`;
