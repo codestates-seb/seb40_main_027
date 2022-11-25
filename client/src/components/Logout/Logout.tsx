b18c927fefcf66d2913b150bcd231de8007eeeee
skyhanull <anky0425@naver.com>
2022-11-25 13:44:15 +0900

    rename:파일위치수정

--- client/src/components/Logout/Logout.tsx
+++ client/src/components/Logout/Logout.tsx
@@ -1,20 +1,7 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import { StyledBackgroundButton } from '../components/Button/BackgroundButton';
+import * as S from './Logout.style';
 
-const LogoutButton = styled(StyledBackgroundButton)`
-  width: 84px;
-  height: 36px;
-  border-radius: 9px;
-  font-size: 20px;
-  @media screen and (max-width: 414px) {
-    width: 70px;
-    height: 30px;
-    border-radius: 4.5px;
-    font-size: 10px;
-  }
-`;
 const Logout = () => {
   const navigate = useNavigate();
 
@@ -31,12 +18,12 @@ const Logout = () => {
         navigate('/');
         alert('로그아웃이 되었습니다');
       })
-      .catch((err) => console.log('err'));
+      .catch(() => console.log('err'));
   };
 
   return (
     <div>
-      <LogoutButton onClick={LogoutHandler}>로그아웃</LogoutButton>
+      <S.LogoutButton onClick={LogoutHandler}>로그아웃</S.LogoutButton>
     </div>
   );
 };
