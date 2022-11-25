1010b39c4c7e68da894e069c31e8ec9e0006313a
skyhanull <anky0425@naver.com>
2022-11-25 13:50:57 +0900

    docs: 코드 수정

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -1,27 +1,11 @@
 import { useRecoilState } from 'recoil';
 import { sideBarFloading } from '../../atoms/index';
-import Logout from '../Logout';
+import Logout from '../Logout/Logout';
 import * as S from './SideBar.style';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
 
 const SideBar = () => {
   const [isContent, setIsContent] = useRecoilState(sideBarFloading);
-
-  const SelectMenu = ['적성검사', '로드맵', '학원일정', '수료후기', '스터디 모집', '멘토링'];
-
-  // useEffect(() => {
-  //   document.body.style.cssText = `
-  //     position: fixed;
-  //     top: -${window.scrollY}px;
-  //     overflow-y: scroll;
-  //     width: 100%;`;
-  //   return () => {
-  //     const scrollY = document.body.style.top;
-  //     document.body.style.cssText = '';
-  //     window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-  //   };
-  // }, []);
 
   function disableScroll() {
     document.body.classList.add('stop-scrolling');
@@ -38,9 +22,6 @@ const SideBar = () => {
           <div className="side-bar-gray-page">
             <div className="side-bar-content">
               <div>
-                {/* {SelectMenu.map((el, idx) => (
-                  <S.SideBarMenu key={idx}>{el}</S.SideBarMenu>
-                ))} */}
                 <S.SideBarMenu>
                   <Link to={'/test'}>적성검사</Link>
                 </S.SideBarMenu>
@@ -59,12 +40,6 @@ const SideBar = () => {
                 <S.SideBarMenu>
                   <Link to={'/mentoring'}>멘토링</Link>
                 </S.SideBarMenu>
-                {/* <Link to={'/test'}>적성검사</Link>
-                <Link to={'/'}>로드맵</Link>
-                <Link to={'/test'}>학원일정</Link>
-                <Link to={'/postscript'}>수료후기</Link>
-                <Link to={'/study'}>스터디</Link>
-                <Link to={'/mentoring'}>멘토링</Link> */}
               </div>
               <S.ButtonSideContent>
                 <Logout />
