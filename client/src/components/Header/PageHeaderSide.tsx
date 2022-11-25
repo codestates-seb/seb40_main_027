bdab6055b164fd8d4a6b9a377f5f6d261f26f153
skyhanull <anky0425@naver.com>
2022-11-25 13:46:41 +0900

    docs:주석삭제

--- client/src/components/Header/PageHeaderSide.tsx
+++ client/src/components/Header/PageHeaderSide.tsx
@@ -1,19 +1,10 @@
 import PageHeader from './PageHeader';
 import SideBar from '../SideBar/SideBar';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { sideBarFloading } from '../../atoms/index';
-import styled from 'styled-components';
-
-// const PageHeaderSideBar = styled.div`
-//   height: 100%;
-//   overflow-y: hidden;
-//   position: fixed;
-//   width: 100%;
-//   touch-action: none;
-// `;
 
 const PageHeaderSide = () => {
-  const [isCollapse, setIsCollapse] = useRecoilState(sideBarFloading);
+  const isCollapse = useRecoilValue(sideBarFloading);
 
   return (
     <div>
