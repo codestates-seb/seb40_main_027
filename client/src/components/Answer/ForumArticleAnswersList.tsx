11c222a1a8790d623870b644591d58a4a9e049ed
skyhanull <anky0425@naver.com>
2022-11-27 01:45:09 +0900

    docs:문서수정

---
+++ client/src/components/Answer/ForumArticleAnswersList.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { useEffect, useState, useRef } from 'react';
+import { useParams, useNavigate } from 'react-router';
+import AnswerListView from './AnswerListView';
+
+interface answerList {
+  postComments: [
+    {
+      createdAt?: string;
+      postCommentId: number;
+      postscriptComment: string;
+      updatedAt?: string;
+    }
+  ];
+}
+
+const ForumArticleAnswerList = () => {
+  const [answerList, setAnswerList] = useState<answerList[]>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.defaults.withCredentials = true;
+
+    axios({
+      method: 'get',
+      url: `/postscript/${id}`,
+    }).then((res) => {
+      const { data } = res;
+      setAnswerList(data.postComments);
+    });
+  }, []);
+
+  return (
+    <div>
+      {answerList?.map((data: any, idx) => (
+        <AnswerListView key={idx} data={data}></AnswerListView>
+      ))}
+    </div>
+  );
+};
+
+export default ForumArticleAnswerList;
