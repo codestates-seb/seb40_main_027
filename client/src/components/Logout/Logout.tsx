271829cb0d93f0938b94b441a98fa7572d91e3c2
skyhanull <anky0425@naver.com>
2022-11-27 02:21:44 +0900

    docs:토큰변경

--- client/src/components/Logout/Logout.tsx
+++ client/src/components/Logout/Logout.tsx
@@ -6,19 +6,22 @@ const Logout = () => {
   const navigate = useNavigate();
 
   const LogoutHandler = () => {
-    axios({
-      method: 'post',
-      url: '/users/logout',
-      headers: {
-        Authorization:
-          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoibGFsYUBnbWFpbC5jb20iLCJzdWIiOiJsYWxhQGdtYWlsLmNvbSIsImlhdCI6MTY2OTE4MTg3NywiZXhwIjoxNjY5MjI1MDc3fQ.YZIFOGbutYKeFh3UKwOcFwfuVu7Azj6waCoh5D_2JmQ84oSdZ7YI3ODtZvHP2pMTydSp9dFK4voAu4moqWKbLw',
-      },
-    })
-      .then(() => {
-        navigate('/');
-        alert('로그아웃이 되었습니다');
+    if (window.confirm('로그아웃을 하시겠습니까?')) {
+      axios({
+        method: 'post',
+        url: '/users/logout',
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoibGFsYUBnbWFpbC5jb20iLCJzdWIiOiJsYWxhQGdtYWlsLmNvbSIsImlhdCI6MTY2OTQ4MTI0OSwiZXhwIjoxNjY5NTI0NDQ5fQ.Fwfii5XFjTe_6W9Zm20h8rDG0Wvrr-EEBOI7sLQl9rJiwNPAPeEJBRk4DkkEAZwoLkeZvgPQZcYHbNBbM1S-TQ',
+        },
       })
-      .catch(() => console.log('err'));
+        .then(() => {
+          navigate('/');
+          console.log('로그아웃이 됨');
+          alert('로그아웃이 되었습니다');
+        })
+        .catch(() => console.log('err'));
+    }
   };
 
   return (
