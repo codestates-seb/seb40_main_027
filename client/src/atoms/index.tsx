da37da9242afb8ede8c967581f0c69096ebfa9fd
skyhanull <anky0425@naver.com>
2022-11-28 19:35:23 +0900

    docs:interface추가

--- client/src/atoms/index.tsx
+++ client/src/atoms/index.tsx
@@ -1,5 +1,22 @@
 import { atom } from 'recoil';
 
+export interface answerListProps {
+  createdAt?: string;
+  postCommentId: number;
+  postscriptComment: string;
+  updatedAt?: string;
+}
+
+interface IanswerList extends Array<answerListProps> {}
+
+// interface answerUserInfoList {
+//   member: {
+//     memberId: number;
+//     email: string;
+//     nickname: string;
+//   };
+// }
+
 export const sideBarFloading = atom({
   key: 'sideBarFloading',
   default: false,
@@ -18,4 +35,9 @@ export const noContent = atom({
 export const isLogin = atom({
   key: 'isLogin',
   default: false,
+});
+
+export const answerListData = atom<IanswerList>({
+  key: 'answerListData',
+  default: [],
 });
