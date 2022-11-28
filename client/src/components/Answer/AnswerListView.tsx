d1842a4a79d2e7a7b60e29ddb474dc9df0e60cd1
skyhanull <anky0425@naver.com>
2022-11-28 19:35:48 +0900

    docs:타입/스타일추가

--- client/src/components/Answer/AnswerListView.tsx
+++ client/src/components/Answer/AnswerListView.tsx
@@ -1,32 +1,40 @@
 import axios from 'axios';
 import DOMPurify from 'dompurify';
-
+import styled from 'styled-components';
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
-import { useRef, useState, useMemo, KeyboardEvent } from 'react';
+import { useRef, useState, useMemo, KeyboardEvent, useCallback, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router';
+import { getComment } from './getApi';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { answerListData, answerListProps } from '../../atoms/index';
 
-interface answerList {
-  list: {
-    createdAt?: string;
-    postCommentId: number;
-    postscriptComment: string;
-    updatedAt?: string;
-  };
-}
+// interface answerListP {
+//   postComments: [
+//     {
+//       createdAt?: string;
+//       postCommentId: number;
+//       postscriptComment: string;
+//       updatedAt?: string;
+//     }
+//   ];
+// }
 
-const AnswerListView = ({ list }: answerList, idx: any) => {
+const AnswerTextContent = styled.div`
+  /* background-color: red; */
+  border-bottom: 1px solid black;
+`;
+
+const AnswerListView = ({ createdAt, postCommentId, updatedAt, postscriptComment }: answerListProps) => {
   //수정상태를 하나만들고 만약맞으면 위에 그...창에서 수정상태를 만든다??
   const [isPatch, setIsPatch] = useState<boolean>(false);
   const [commentInfo, setCommentInfo] = useState([]);
   const [commentValue, setCommentValue] = useState('');
+  const [answerList, setAnswerList] = useRecoilState(answerListData);
+
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const CommentValueHandler = (event: any) => {
-    setCommentValue(event);
-    console.log(commentValue);
-  };
   const modules = useMemo(
     () => ({
       toolbar: {
@@ -46,69 +54,110 @@ const AnswerListView = ({ list }: answerList, idx: any) => {
     setIsPatch(!isPatch);
   };
 
-  const PatchHanlder = () => {
-    //   axios({
-    //     method: 'patch',
-    //     url: `/postscript/comment/${list.postCommentId}`,
-    //     data: { commentValue },
-    //     headers: {
-    //       Authorization:
-    //         'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoibGFsYUBnbWFpbC5jb20iLCJzdWIiOiJsYWxhQGdtYWlsLmNvbSIsImlhdCI6MTY2OTQ0OTUxNSwiZXhwIjoxNjY5NDkyNzE1fQ.SbuTBucG_fvPnESoQvuBunGpmI3283d9OH1XXVnR2dsmcgiGwtbGDonfzRxiWZSZvY1GmBXxFT3Dob56QLs3lQ',
-    //     },
-    //   })
-    //     .then((res) => {
-    //       console.log(res);
-    //       const { data } = res;
-    //       setCommentInfo(data);
-    //       setIsPatch(!isPatch);
-    //     })
-    //     .catch(() => console.log('err'));
-    // };
-    axios
-      .patch(`/postscript/comment/${list.postCommentId}`, { postCommentContent: commentValue })
-      .then((res) => {
-        const { data } = res;
-        setCommentInfo(data);
-        setIsPatch(!isPatch);
-      })
-      .catch(() => console.log('err'));
+  const patchComment = () => {
+    return axios({
+      method: 'patch',
+      url: `/postscript/comment/${postCommentId}`,
+      data: { postscriptComment: commentValue },
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoicGp3QGdtYWlsLmNvbSIsInN1YiI6InBqd0BnbWFpbC5jb20iLCJpYXQiOjE2Njk2MDk1NDMsImV4cCI6MTY2OTY5NTk0M30.XcAWYYmpkTNFhq-VaW8zKthvWNlBYWjsTAUf2eXoL_Zz0WGL0DO5YD6vKC8B6ofsbYhRz4KgTZcoLlAVvikxMQ',
+      },
+    });
   };
 
-  const deleteHandler = () => {
-    axios({
+  const deleteComment = () => {
+    return axios({
       method: 'delete',
-      url: `/postscript/comment/delete/${list.postCommentId}`,
+      url: `/postscript/comment/delete/${postCommentId}`,
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoicGp3QGdtYWlsLmNvbSIsInN1YiI6InBqd0BnbWFpbC5jb20iLCJpYXQiOjE2Njk2MDk1NDMsImV4cCI6MTY2OTY5NTk0M30.XcAWYYmpkTNFhq-VaW8zKthvWNlBYWjsTAUf2eXoL_Zz0WGL0DO5YD6vKC8B6ofsbYhRz4KgTZcoLlAVvikxMQ',
+      },
     })
-      .then(() => {
-        window.location.reload();
-      })
+      .then(() => {})
       .catch(() => console.log('err'));
   };
 
-  //{list.postscriptComment}
+  const patchAsync = async () => {
+    axios.defaults.withCredentials = true;
+    try {
+      const patchAwait = await patchComment();
+      const getAwait = await getComment(`${id}`);
+
+      setAnswerList(getAwait.data.postComments);
+    } catch {
+      console.log('err');
+    }
+  };
+
+  const deleteAsync = async () => {
+    axios.defaults.withCredentials = true;
+    try {
+      const deleteAwait = await deleteComment();
+      const getAwait = await getComment(`${id}`);
+
+      setAnswerList(getAwait.data.postComments);
+    } catch {
+      console.log('err');
+    }
+  };
+
+  const PatchHanlder = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    //수정에서 403오류 발생
+    patchAsync();
+    // axios({
+    //   method: 'patch',
+    //   url: `/postscript/comment/${list.postCommentId}`,
+    //   data: { commentValue },
+    //   headers: {
+    //     Authorization:
+    //       'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoibGFsYUBnbWFpbC5jb20iLCJzdWIiOiJsYWxhQGdtYWlsLmNvbSIsImlhdCI6MTY2OTQ0OTUxNSwiZXhwIjoxNjY5NDkyNzE1fQ.SbuTBucG_fvPnESoQvuBunGpmI3283d9OH1XXVnR2dsmcgiGwtbGDonfzRxiWZSZvY1GmBXxFT3Dob56QLs3lQ',
+    //   },
+    // })
+    //   .then((res) => {
+    //     console.log(res);
+    //     const { data } = res;
+    //     setCommentInfo(data);
+    //     setIsPatch(!isPatch);
+    //   })
+    //   .catch(() => console.log('err'));
+  };
+
+  const deleteHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    deleteAsync();
+  };
+
   return (
-    <div>
+    <AnswerTextContent>
       {isPatch ? (
         <div>
-          <ReactQuill theme="snow" value={commentValue} onChange={CommentValueHandler} />
+          <ReactQuill theme="snow" value={commentValue} onChange={setCommentValue} />
           <button onClick={PatchHanlder}>완료</button>
         </div>
       ) : (
-        'a'
+        <div>
+          <div>
+            <div>
+              <span>username</span>
+              <div>
+                <button onClick={editHandler}>수정</button>
+                <button onClick={deleteHandler}>삭제</button>
+              </div>
+            </div>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(postscriptComment),
+              }}
+            />
+          </div>
+        </div>
       )}
-      {commentInfo.length !== 0 ? (
-        <div>{commentInfo}</div>
-      ) : (
-        <div
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(list.postscriptComment),
-          }}
-        />
-      )}
-      <div>{list.postCommentId}</div>
-      <button onClick={editHandler}>수정</button>
-      <button onClick={deleteHandler}>삭제</button>
-    </div>
+
+      <div>{postCommentId}</div>
+    </AnswerTextContent>
   );
 };
 
