a01bc205597691413482709920ed857975a59ca8
pjnw1236 <pjnw1236@gmail.com>
2022-11-30 14:15:58 +0900

    fix JwtVerificationfilter

--- server/src/main/java/com/yes27/auth/jwt/JwtVerificationFilter.java
+++ server/src/main/java/com/yes27/auth/jwt/JwtVerificationFilter.java
@@ -10,7 +10,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
