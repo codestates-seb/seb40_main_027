304130ecc891e3d3ce4dff3c61215faca015c481
skyhanull <anky0425@naver.com>
2022-11-30 11:10:06 +0900

    docs:토큰,인자 조정

--- client/src/utils/API/getApi.tsx
+++ client/src/utils/API/getApi.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const getComment = (id: string) => {
+export const getComment = (url: string, id: string) => {
   return axios({
     method: 'get',
-    url: `/postscript/${id}`,
+    url: `/${url}/${id}`,
     headers: {
       Authorization:
-        'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoicGp3QGdtYWlsLmNvbSIsInN1YiI6InBqd0BnbWFpbC5jb20iLCJpYXQiOjE2Njk2MDk1NDMsImV4cCI6MTY2OTY5NTk0M30.XcAWYYmpkTNFhq-VaW8zKthvWNlBYWjsTAUf2eXoL_Zz0WGL0DO5YD6vKC8B6ofsbYhRz4KgTZcoLlAVvikxMQ',
+        'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoiYWJjZEBnbWFpbC5jb20iLCJzdWIiOiJhYmNkQGdtYWlsLmNvbSIsImlhdCI6MTY2OTczNzAwNSwiZXhwIjoxNjY5ODIzNDA1fQ.AykpiUvJlzmcTWT7x2iMKPbPo0y9cCIVzqhiMECTGFKAMKg171ropdOZjpB_lLbV7m6AkQBlYPbIahmpmPGcdQ',
     },
   });
 };
