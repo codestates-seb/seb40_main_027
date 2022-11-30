ef39686bc2ae335fee411c0cf4674f0aa7c201db
didwndbs <didwndbs@naver.com>
2022-11-30 11:31:11 +0900

    fix: 부트캠프 찜 목록 조회 코드 수정

--- server/src/main/java/com/yes27/bootcamp/repository/BootcampRepository.java
+++ server/src/main/java/com/yes27/bootcamp/repository/BootcampRepository.java
@@ -4,6 +4,10 @@ import com.yes27.bootcamp.entity.BootCamp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.swing.*;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface BootcampRepository extends JpaRepository<BootCamp, Long> {
+
+    List<BootCamp> findAllByFinalRegisterDateLessThanEqual(LocalDate now);
 }
