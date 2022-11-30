c3051d635bd1555fffff789203f5ff90a561c2ac
youngjun kim <108052178+kyj4327@users.noreply.github.com>
2022-11-30 14:43:27 +0900

    fix : Mypage_Response 수정 member-mentor #202

--- server/src/main/java/com/yes27/mentoring/mapper/MentorMapper.java
+++ server/src/main/java/com/yes27/mentoring/mapper/MentorMapper.java
@@ -1,6 +1,7 @@
 package com.yes27.mentoring.mapper;
 
 
+import com.yes27.member.mapper.MemberMapper;
 import com.yes27.mentoring.dto.MentorDto;
 import com.yes27.mentoring.entity.Mentor;
 import com.yes27.mentoringLike.entity.MentoringVote;
@@ -8,6 +9,7 @@ import com.yes27.mentoringcomment.entity.Comment;
 import com.yes27.mentoringcomment.dto.CommentDto;
 import org.mapstruct.Mapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -54,4 +56,28 @@ public interface MentorMapper {
     }
 
 
+    default List<MentorDto.MentorMypageResponse> mentorsToMypageResponse(List<Mentor> mentors, MemberMapper memberMapper){
+        if(mentors == null) return null;
+        List<MentorDto.MentorMypageResponse> mentorMypageResponsesDtos = new ArrayList<>(mentors.size());
+
+        for(Mentor mentor : mentors) {
+            mentorMypageResponsesDtos.add(mentorToMypageResponseDto(mentor,memberMapper));
+        }
+        return mentorMypageResponsesDtos;
+    }
+
+    default MentorDto.MentorMypageResponse mentorToMypageResponseDto(Mentor mentor,MemberMapper memberMapper){
+
+        MentorDto.MentorMypageResponse mentorMypageResponse = new MentorDto.MentorMypageResponse();
+        mentorMypageResponse.setMentoringId(mentor.getMentoringId());
+        mentorMypageResponse.setMentoringTitle(mentor.getMentoringTitle());
+        mentorMypageResponse.setMentoringContent(mentor.getMentoringContent());
+        mentorMypageResponse.setView(mentor.getViewCount());
+        mentorMypageResponse.setTotalVotes(mentor.getTotalVotes());
+        mentorMypageResponse.setTagName(mentor.getTagName());
+        mentorMypageResponse.setCreatedAt(mentor.getCreatedAt());
+        mentorMypageResponse.setUpdatedAt(mentor.getUpdatedAt());
+
+        return mentorMypageResponse;
+    }
 }
