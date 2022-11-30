46b86e73617de47068c3c1ffdeb4625b5a2186d2
skyhanull <anky0425@naver.com>
2022-11-30 15:15:14 +0900

    refactor: css분리,코드 분리

--- client/src/components/Answer/AnswerListView.tsx
+++ client/src/components/Answer/AnswerListView.tsx
@@ -1,89 +1,35 @@
 import axios from 'axios';
 import DOMPurify from 'dompurify';
-import styled from 'styled-components';
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 import { useState, useMemo } from 'react';
 import { useParams } from 'react-router';
 import { getComment } from '../../utils/API/getApi';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-// import { answerListData, answerListProps } from '../../atoms/index';
+import { useSetRecoilState } from 'recoil';
 import { answerListData } from '../../atoms/index';
-import { StyledBorderButton } from '../Button/BorderButton';
-import { Icon } from '@iconify/react';
-import { StyledBackgroundButton } from '../Button/BackgroundButton';
 import { formatDistanceToNow, format } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { Calculate } from '../Calculate';
-
-const AnswerTextContent = styled.div`
-  border-bottom: 1px solid black;
-
-  &:nth-last-child(1) {
-    border: none;
-  }
-`;
-
-const UserAnswerInfo = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const TimeOrName = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-top: 0.5rem;
-
-  span:nth-last-child(1) {
-    margin-left: 1rem;
-    margin-top: 0;
-    align-items: flex-start;
-  }
-`;
-
-const AnswerButton = styled(StyledBorderButton)`
-  margin: 0.5rem;
-`;
-
-const OkButton = styled(StyledBackgroundButton)`
-  width: 3rem;
-  height: 2rem;
-  margin-right: 0.5rem;
-`;
-
-const ButtonArea = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-`;
-
-const NameZone = styled.span`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: row;
-`;
-
-const TextArea = styled.div`
-  height: 5vh;
-`;
+import * as S from './AnswerListView.style';
+import { AnswerViewContainer, QuillContainer } from './AnswerContainer';
 
 interface answerListProps {
   createdAt: string;
   postCommentId: number;
   postscriptComment: string;
-  updatedAt?: string;
+  updatedAt: string;
   nickname: string;
 }
 
 const AnswerListView = ({ createdAt, postCommentId, updatedAt, postscriptComment, nickname }: answerListProps) => {
   const [isPatch, setIsPatch] = useState<boolean>(false);
   const [commentValue, setCommentValue] = useState('');
-  const [answerList, setAnswerList] = useRecoilState(answerListData);
+  // const [answerList, setAnswerList] = useRecoilState(answerListData);
+  const setAnswerList = useSetRecoilState(answerListData);
 
   const ds = new Date(createdAt);
   const d = format(ds, 'yyyy.MM.dd HH:mm:ss');
-  // console.log(Calculate(createdAt));
+
   let timels = Calculate(createdAt);
   const createTime = formatDistanceToNow(new Date(d), { locale: ko });
   // const updateTime = formatDistanceToNow(new Date(updatedAt), { locale: ko });
@@ -170,45 +116,29 @@ const AnswerListView = ({ createdAt, postCommentId, updatedAt, postscriptComment
   };
 
   return (
-    <AnswerTextContent>
+    <S.AnswerTextContent>
       {isPatch ? (
         <div>
           <ReactQuill theme="snow" value={commentValue} onChange={setCommentValue} />
-          <ButtonArea>
-            <OkButton onClick={PatchHanlder}>완료</OkButton>
-            <OkButton color={'red'} onClick={editHandler}>
-              취소
-            </OkButton>
-          </ButtonArea>
+          <QuillContainer PatchHanlder={PatchHanlder} editHandler={editHandler} />
         </div>
       ) : (
         <div>
-          <div>
-            <UserAnswerInfo>
-              <TimeOrName>
-                <NameZone>
-                  <Icon icon="carbon:user-avatar-filled-alt" width="20" height="15" />
-                  {nickname}
-                </NameZone>
-                <span>{updatedAt ? <span>{timels}</span> : createTime} </span>
-              </TimeOrName>
-
-              <div>
-                <AnswerButton onClick={editHandler}>수정</AnswerButton>
-                <AnswerButton color="red" onClick={deleteHandler}>
-                  삭제
-                </AnswerButton>
-              </div>
-            </UserAnswerInfo>
-            <TextArea
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(postscriptComment),
-              }}
-            />
-          </div>
+          <AnswerViewContainer
+            nick={nickname}
+            updateAt={updatedAt}
+            createAt={createdAt}
+            editHandler={editHandler}
+            deleteHandler={deleteHandler}
+          />
+          <S.TextArea
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(postscriptComment),
+            }}
+          />
         </div>
       )}
-    </AnswerTextContent>
+    </S.AnswerTextContent>
   );
 };
 
