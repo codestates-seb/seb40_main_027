c937317a2c445ab23d8f8d74d0ddbcc59921cb4f
didwndbs <didwndbs@naver.com>
2022-11-30 11:28:32 +0900

    fix: 상세 조회시 좋아요 유무 확인가능하도록 수정,

--- server/src/main/java/com/yes27/bootcamp/service/BootcampService.java
+++ server/src/main/java/com/yes27/bootcamp/service/BootcampService.java
@@ -4,6 +4,9 @@ import com.yes27.bootcamp.entity.BootCamp;
 import com.yes27.bootcamp.repository.BootcampRepository;
 import com.yes27.exception.BusinessLogicException;
 import com.yes27.exception.ExceptionCode;
+import com.yes27.member.entity.Member;
+import com.yes27.mybootcamp.entity.Mybootcamp;
+import com.yes27.mybootcamp.repository.MybootcampRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -16,41 +19,77 @@ public class BootcampService {
 
     private final BootcampRepository bootcampRepository;
 
-    public BootcampService(BootcampRepository bootcampRepository) {
+    private final MybootcampRepository mybootcampRepository;
+
+    public BootcampService(BootcampRepository bootcampRepository, MybootcampRepository mybootcampRepository) {
         this.bootcampRepository = bootcampRepository;
+        this.mybootcampRepository = mybootcampRepository;
     }
 
-    public BootCamp update(BootCamp bootCamp, Long bootcampId){
-        BootCamp findCamp = findBootcamp(bootcampId);
-//        BootCamp findCamp = findVerifiedBootCamp(bootCamp.getBootcampId());
-        findCamp.setDuration(findCamp.getDuration());
-        findCamp.setBeginRegisterDate(findCamp.getBeginRegisterDate());
-        findCamp.setFinalRegisterDate(findCamp.getFinalRegisterDate());
-        findCamp.setSite(findCamp.getSite());
-        findCamp.setOnOff(findCamp.getOnOff());
-        findCamp.setProcess(findCamp.getProcess());
-        findCamp.setSatisfaction(findCamp.getSatisfaction());
-        findCamp.setEndDate(findCamp.getEndDate());
-        findCamp.setSuperviser(findCamp.getSuperviser());
-        findCamp.setTotalCost(findCamp.getTotalCost());
-        findCamp.setTitle(findCamp.getTitle());
-        findCamp.setTrTime(findCamp.getTrTime());
-        findCamp.setStartDate(findCamp.getStartDate());
-        findCamp.setWeekendStatus(findCamp.getWeekendStatus());
+    public BootCamp update(BootCamp bootCamp){
+        BootCamp findCamp = findBootcamp(bootCamp.getBootcampId());
+
+        Optional.ofNullable(findCamp.getDuration())
+                .ifPresent(duration -> bootCamp.setDuration(duration));
+        Optional.ofNullable(findCamp.getBeginRegisterDate()).
+                ifPresent(beginRegisterDate->bootCamp.setBeginRegisterDate(beginRegisterDate));
+        Optional.ofNullable(findCamp.getFinalRegisterDate())
+                        .ifPresent(finalRegisterDate->bootCamp.setFinalRegisterDate(finalRegisterDate));
+        Optional.ofNullable(findCamp.getSite())
+                        .ifPresent(site -> bootCamp.setSite(site));
+        Optional.ofNullable(findCamp.getOnOff())
+                        .ifPresent(onOff ->bootCamp.setOnOff(onOff));
+        Optional.ofNullable(findCamp.getProcess())
+                        .ifPresent(process -> bootCamp.setProcess(process));
+        Optional.ofNullable(findCamp.getSatisfaction())
+                        .ifPresent(satisfaction->bootCamp.setSatisfaction(satisfaction));
+        Optional.ofNullable(findCamp.getEndDate())
+                        .ifPresent(endDate -> bootCamp.setEndDate(endDate));
+        Optional.ofNullable(findCamp.getSuperviser())
+                        .ifPresent(superviser->bootCamp.setSuperviser(superviser));
+        Optional.ofNullable(findCamp.getTotalCost())
+                        .ifPresent(totalCost->bootCamp.setTotalCost(totalCost));
+        Optional.ofNullable(findCamp.getTitle())
+                        .ifPresent(title->bootCamp.setTitle(title));
+        Optional.ofNullable(findCamp.getTrTime())
+                        .ifPresent(trTime->bootCamp.setTrTime(trTime));
+        Optional.ofNullable(findCamp.getStartDate())
+                        .ifPresent(startDate->bootCamp.setStartDate(startDate));
+        Optional.ofNullable(findCamp.getWeekendStatus())
+                        .ifPresent(weekendStatus->bootCamp.setWeekendStatus(weekendStatus));
+        Optional.ofNullable(findCamp.getYardMan())
+                        .ifPresent(yardMan->bootCamp.setYardMan(yardMan));
+        Optional.ofNullable(findCamp.getAddress())
+                        .ifPresent(address -> bootCamp.setAddress(address));
 
         return bootcampRepository.save(findCamp);
     }
     public Page<BootCamp> findBootcamps(int page, int size){
+//        updateDate();
         return bootcampRepository.findAll(PageRequest.of(page,size, Sort.by("bootcampId").descending()));
     }
 
     public Page<BootCamp> findBootscamp(int page, int size, String sort){
+        if(sort.equals("finalRegisterDate")){
+            return bootcampRepository.findAll(PageRequest.of(page, size, Sort.by(sort).ascending()));
+        }
         return bootcampRepository.findAll(PageRequest.of(page, size, Sort.by(sort).descending()));
     }
 
     public BootCamp findBootcamp(Long bootcampId){
        BootCamp findCamp = findVerifiedBootCamp(bootcampId);
+
        return findCamp;
+    }
+    // 좋아요 유무
+    public int isVote(Long bootcampId, Member member){
+        BootCamp findCamp = findBootcamp(bootcampId);
+        Optional<Mybootcamp> isVote = mybootcampRepository.findByBootCampAndMember(findCamp,member);
+        if(isVote.isPresent()){
+            return isVote.get().getVote();
+        }else{
+            return 0;
+        }
     }
 
     public BootCamp findVerifiedBootCamp(Long bootcampId) {
@@ -67,6 +106,17 @@ public class BootcampService {
         bootcampRepository.delete(findCamp);
     }
 
+    //beginRegister상태변경하기
+//    public void updateDate(){
+//        List<BootCamp> findList = bootcampRepository.findAllByFinalRegisterDateLessThanEqual(LocalDate.now());
+//        List<BootCamp> updateCamp = new ArrayList<>();
+//        for(int i = 0 ; i<findList.size(); i++){
+//            BootCamp findCamp = findBootcamp(findList.get(i).getBootcampId());
+//           findCamp.setBeginRegisterDate("모집마감");
+//           updateCamp.add(i,findCamp);
+//        }
+//        bootcampRepository.saveAll(updateCamp);
+//    }
 
 
 }
