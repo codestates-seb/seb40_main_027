f61591a3db3e5796e9d97f72a61cd7a72252ee61
skyhanull <anky0425@naver.com>
2022-11-30 11:09:09 +0900

    design:댓글창 css

--- client/src/components/Answer/ForumArticlesAnswer.tsx
+++ client/src/components/Answer/ForumArticlesAnswer.tsx
@@ -8,16 +8,41 @@ import 'react-quill/dist/quill.snow.css';
 import { getComment } from '../../utils/API/getApi';
 import { useRecoilState } from 'recoil';
 import { answerListData } from '../../atoms/index';
+import { StyledBackgroundButton } from '../Button/BackgroundButton';
 
 const QuillContent = styled.div`
-  /* border: 3px solid red;
-  display: flex;
-  justify-content: center;
-  height: 18vh;
-  flex-direction: column;
-  flex-wrap: wrap; */
-  /* background-color: gray; */
+  border: 2px solid var(--greenSub);
+  border-radius: 10px;
+  height: 16vh;
+
+  .btn-area {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
 `;
+const QuillArea = styled(ReactQuill)`
+  background-color: var(--whiteBackground);
+  margin: 1rem;
+  border-radius: 10px;
+`;
+
+const SubmitButtton = styled(StyledBackgroundButton)`
+  width: 6rem;
+  height: 2rem;
+  margin-right: 2rem;
+  border-radius: 10px;
+`;
+
+interface answerListProps {
+  createdAt: string;
+  postCommentId: number;
+  postscriptComment: string;
+  updatedAt?: string;
+  nickname: string;
+}
+
+export interface IanswerList extends Array<answerListProps> {}
 
 const ForumArticlesAnswer = () => {
   const quillRef = useRef<ReactQuill>();
@@ -49,7 +74,7 @@ const ForumArticlesAnswer = () => {
       data: { postscriptComment: answerContents },
       headers: {
         Authorization:
-          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoicGp3QGdtYWlsLmNvbSIsInN1YiI6InBqd0BnbWFpbC5jb20iLCJpYXQiOjE2Njk2OTU4MzYsImV4cCI6MTY2OTc4MjIzNn0.P6yVNzmbVyPjAe5RlYE3i-7LG-hvHiCOSP0dPFnsaGVa9BiwqVQ0UVCewVlfmsWkYHdNF5Wg2zI9CEs6EnKxVg',
+          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoiYWJjZEBnbWFpbC5jb20iLCJzdWIiOiJhYmNkQGdtYWlsLmNvbSIsImlhdCI6MTY2OTcwNDAyMSwiZXhwIjoxNjY5NzkwNDIxfQ.ipJnckImRyPfR9kXlDI3Kajkp-M3RZzFUHBDpdxBK1Teu0kV8wjyHxh6WET_fckelUSByRdh7QDTZOnqA8FFXg',
       },
     });
   };
@@ -58,7 +83,7 @@ const ForumArticlesAnswer = () => {
     axios.defaults.withCredentials = true;
     try {
       const postAwait = await postComment();
-      const getAwait = await getComment(`${id}`);
+      const getAwait = await getComment('postscript', `${id}`);
       setAnswerList(getAwait.data.postComments);
       // setAnswerUserName(getAwait.data.member);
       // console.log(answerUserName);
@@ -92,13 +117,15 @@ const ForumArticlesAnswer = () => {
             postCommentId={list.postCommentId}
             postscriptComment={list.postscriptComment}
             updatedAt={list.updatedAt}
+            nickname={list.nickname}
           ></AnswerListView>
         ))}
       </div>
       <QuillContent>
-        <ReactQuill theme="snow" value={answerContents} onChange={(e) => setAnswerContents(e)} />
-
-        <button onClick={SummitAnswerBtn}>추가</button>
+        <QuillArea theme="snow" value={answerContents} onChange={(e) => setAnswerContents(e)} />
+        <div className="btn-area">
+          <SubmitButtton onClick={SummitAnswerBtn}>등록</SubmitButtton>
+        </div>
       </QuillContent>
     </>
   );
