9cf32a578bdbeb2173942abe395771a3496188d8
skyhanull <anky0425@naver.com>
2022-12-02 01:09:51 +0900

    fix:파일명 변경 및 에러 코드 구현

--- client/src/components/Answer/AnswerListView.tsx
+++ client/src/components/Answer/AnswerListView.tsx
@@ -4,7 +4,7 @@ import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 import { useState } from 'react';
 import { useParams } from 'react-router';
-import { getComment, deleteComment } from '../../utils/api/getApi';
+import { getComment, deleteComment } from '../../utils/api/answerAPI';
 import { useSetRecoilState } from 'recoil';
 import { answerListData } from '../../atoms/index';
 import * as S from './AnswerListView.style';
@@ -47,26 +47,32 @@ const AnswerListView = ({
   };
 
   const patchAsync = async () => {
-    axios.defaults.withCredentials = true;
     try {
       await patchComment();
       const getAwait = await getComment('postscript', `${id}`);
 
       setAnswerList(getAwait.data.data.postscriptComments);
-    } catch {
-      console.log('err');
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        console.log(error);
+      }
     }
   };
 
   const deleteAsync = async () => {
-    axios.defaults.withCredentials = true;
     try {
       await deleteComment(`${postscriptCommentId}`, 'postscript');
       const getAwait = await getComment('postscript', `${id}`);
 
       setAnswerList(getAwait.data.data.postscriptComments);
-    } catch {
-      console.log('err');
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        console.log(error);
+      }
     }
   };
 
@@ -86,8 +92,13 @@ const AnswerListView = ({
     <S.AnswerTextContent>
       {isPatch ? (
         <div>
-          <ReactQuill theme="snow" value={commentValue} onChange={setCommentValue} />
-          <QuillContainer PatchHanlder={PatchHanlder} editHandler={editHandler} />
+          <ReactQuill
+            theme="snow"
+            value={commentValue}
+            onChange={setCommentValue}
+            placeholder={`${postscriptComment}`}
+          />
+          <QuillContainer patchHandler={PatchHanlder} editHandler={editHandler} />
         </div>
       ) : (
         <div>
