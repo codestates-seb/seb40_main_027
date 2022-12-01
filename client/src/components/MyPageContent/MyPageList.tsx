db1806f02e20ab15268b5210d3e9d58c7416261d
skyhanull <anky0425@naver.com>
2022-12-01 09:47:36 +0900

    docs:더미데이터 삭제

--- client/src/components/MyPageContent/MyPageList.tsx
+++ client/src/components/MyPageContent/MyPageList.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import MyPageLists from './MyPageLists';
+import axios from 'axios';
+import { SetStateAction, useCallback, useEffect, useState } from 'react';
+import MyPagePagination from './MyPagePagination';
 
 const ListContent = styled.div`
   height: 90vh;
@@ -28,115 +31,111 @@ const ListContent = styled.div`
     }
   }
 `;
+interface PropsAnswerType {
+  mentoringId?: number;
+  mentoringTitle?: string;
+  mentoringContent?: string;
+  postscriptId?: number;
+  postscriptTitle?: string;
+  postscriptContent?: string;
+  studyId?: number;
+  studyTitle?: string;
+  studyContent?: string;
+  totalVotes: number;
+  createdAt: string;
+  updatedAt: string;
+  tagName: string;
+  view: number;
+}
 
-const dummyDatalist = [
-  {
-    postscriptId: 1,
-    postscriptTitle: '코드스테이츠 프론트엔드 후기',
-    postscriptContent:
-      '<p>안녕하세요. 코드스테이츠 프론트엔드 부트캠프 후기입니다. 저는 수료 후 네카라쿠배 중 1곳에 취업했습니다.</p><h3>부트캠프 활용 꿀팁</h3><p>동기들과의 스터디에 적극 참여하세요.<br>매일 알고리즘 문제 1개 이상 풀기</p>',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    view: 32,
-    like: 2,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-20T16:35:41.87709',
-    updatedAt: '2022-11-20T16:35:41.87709',
-  },
-  {
-    postscriptId: 2,
-    postscriptTitle: '테스트용 제목2.',
-    postscriptContent: '테스트 내용2 ',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    like: 0,
-    view: 5,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-18T16:35:41.87709',
-    updatedAt: '2022-11-18T16:35:41.87709',
-  },
-  {
-    postscriptId: 3,
-    postscriptTitle: '테스트용 제목3.',
-    postscriptContent: '테스트 내용2 ',
-
-    like: 0,
-    view: 10,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-18T16:35:41.87709',
-    updatedAt: '2022-11-18T16:35:41.87709',
-  },
-  {
-    postscriptId: 1,
-    postscriptTitle: '코드스테이츠 프론트엔드 후기',
-    postscriptContent:
-      '<p>안녕하세요. 코드스테이츠 프론트엔드 부트캠프 후기입니다. 저는 수료 후 네카라쿠배 중 1곳에 취업했습니다.</p><h3>부트캠프 활용 꿀팁</h3><p>동기들과의 스터디에 적극 참여하세요.<br>매일 알고리즘 문제 1개 이상 풀기</p>',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    view: 32,
-    like: 2,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-10-20T16:35:41.87709',
-  },
-
-  {
-    postscriptId: 1,
-    postscriptTitle: '코드스테이츠 프론트엔드 후기',
-    postscriptContent:
-      '<p>안녕하세요. 코드스테이츠 프론트엔드 부트캠프 후기입니다. 저는 수료 후 네카라쿠배 중 1곳에 취업했습니다.</p><h3>부트캠프 활용 꿀팁</h3><p>동기들과의 스터디에 적극 참여하세요.<br>매일 알고리즘 문제 1개 이상 풀기</p>',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    view: 32,
-    like: 2,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-20T16:35:41.87709',
-    updatedAt: '2022-11-20T16:35:41.87709',
-  },
-  {
-    postscriptId: 1,
-    postscriptTitle: '코드스테이츠 프론트엔드 후기',
-    postscriptContent:
-      '<p>안녕하세요. 코드스테이츠 프론트엔드 부트캠프 후기입니다. 저는 수료 후 네카라쿠배 중 1곳에 취업했습니다.</p><h3>부트캠프 활용 꿀팁</h3><p>동기들과의 스터디에 적극 참여하세요.<br>매일 알고리즘 문제 1개 이상 풀기</p>',
-    postscriptTags: ['부트캠프 후기', '개발 공부법'],
-    view: 32,
-    like: 2,
-    user: {
-      userId: 0,
-      name: '홍길동',
-      userEmail: 'hong@gmail.com',
-    },
-    createdAt: '2022-11-20T16:35:41.87709',
-    updatedAt: '2022-11-20T16:35:41.87709',
-  },
-];
+export interface propsAnswerList extends Array<PropsAnswerType> {}
 
 const MyPageList = () => {
+  // const [fWritingList, setFWritingList] = useState([]);
+  // const [sWritingList, setSWritingList] = useState([]);
+  // const [mWritingList, setMWritingList] = useState([]);
+  const [totalPost, setTotalPost] = useState(0);
+  const [currentPosts, setCurrentPosts] = useState<PropsAnswerType[]>();
+  const [page, setPage] = useState(1);
+  const [postPerPage] = useState(7);
+  const indexOfLastPost = page * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+
+  const handlePostPage = (page: number) => {
+    setPage(page);
+  };
+
+  const plusClick = (e: string) => {
+    setPage(page + 1);
+  };
+
+  const MinusClick = (e: string) => {
+    setPage(page - 1);
+  };
+  console.log(page);
+
+  useEffect(() => {
+    axios.defaults.withCredentials = true;
+    axios({
+      method: 'get',
+      url: '/users/mypage/writing',
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImVtYWlsIjoiYWJjZEBnbWFpbC5jb20iLCJzdWIiOiJhYmNkQGdtYWlsLmNvbSIsImlhdCI6MTY2OTc5Mzk4OSwiZXhwIjoxNjY5ODgwMzg5fQ.jwHPEroDLyZmiL2TRvYuvaED2bCmDwsXUs3QL1nA8QVoU9Kkr2nmJd5vSlHdOr5ak9hKHOf3Vj65Ffe_gIUdnQ',
+      },
+    }).then((res) => {
+      const { data } = res;
+      // setFWritingList(data.data.postscript.reverse());
+      // setSWritingList(data.data.studies);
+      // setMWritingList(data.data.mentor);
+      const postP = data.data.postscript;
+      const postS = data.data.studies;
+      const postM = data.data.mentor;
+      // const Maxarr = [...data.data.studies, ...sWritingList, ...mWritingList];
+      const Maxarr: propsAnswerList = [...postP, ...postS, ...postM];
+
+      setTotalPost(postP.length + postM.length + postS.length);
+      const MaxPage = Maxarr.slice(indexOfFirstPost, indexOfLastPost);
+      setCurrentPosts(MaxPage);
+    });
+  }, [page, indexOfFirstPost, indexOfLastPost]);
+
   return (
     <ListContent>
       <div className="list-line">
         <span className="list-name">작성 글</span>
       </div>
       <div className="content-list">
-        {dummyDatalist.map((list, idx) => (
-          <MyPageLists key={idx} list={list} />
-        ))}
+        {currentPosts &&
+          currentPosts.map((list, idx) => (
+            <MyPageLists
+              key={idx}
+              mentoringId={list.mentoringId}
+              mentoringTitle={list.mentoringTitle}
+              mentoringContent={list.mentoringContent}
+              postscriptId={list.postscriptId}
+              postscriptTitle={list.postscriptTitle}
+              postscriptContent={list.postscriptContent}
+              studyId={list.studyId}
+              studyTitle={list.studyTitle}
+              studyContent={list.studyContent}
+              totalVotes={list.totalVotes}
+              createdAt={list.createdAt}
+              updatedAt={list.updatedAt}
+              tagName={list.tagName}
+              view={list.view}
+            />
+          ))}
       </div>
+      <MyPagePagination
+        totalCount={totalPost}
+        page={page}
+        postPerPage={postPerPage}
+        pageRangeDisplayed={5}
+        handlePageChange={handlePostPage}
+        plusClick={plusClick}
+        MinusClick={MinusClick}
+      />
     </ListContent>
   );
 };
