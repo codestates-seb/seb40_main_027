16e02271d50deae71de4e817c93fca5fb71def5e
didwndbs <didwndbs@naver.com>
2022-12-01 16:56:10 +0900

    feat: 마이페이지- 부트캠프 찜 목록 정렬, 페이징 구현

--- server/src/main/java/com/yes27/mybootcamp/mapper/MybootcampMapper.java
+++ server/src/main/java/com/yes27/mybootcamp/mapper/MybootcampMapper.java
@@ -6,6 +6,7 @@ import com.yes27.bootcamp.entity.BootCamp;
 import com.yes27.member.entity.Member;
 import com.yes27.mybootcamp.dto.MybootcampDto;
 import com.yes27.mybootcamp.entity.Mybootcamp;
+import com.yes27.mybootcamp.entity.MybootcampMapping;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -14,17 +15,17 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public interface MybootcampMapper {
 
-    default List<BootcampDto.Response> bootcampToMyBootcampResponse(List<BootCamp> bootCamps){
+    default List<BootcampDto.Response> bootcampToMyBootcampResponse(List<MybootcampMapping> bootCamps){
         return bootCamps.stream()
                 .map(mybootcamp -> BootcampDto.Response
                         .builder()
-                        .bootcampId(mybootcamp.getBootcampId())
-                        .totalCost(mybootcamp.getTotalCost())
-                        .title(mybootcamp.getTitle())
-                        .process(mybootcamp.getProcess())
-                        .onOff(mybootcamp.getOnOff())
-                        .beginRegisterDate(mybootcamp.getBeginRegisterDate())
-                        .finalRegisterDate(mybootcamp.getFinalRegisterDate())
+                        .bootcampId(mybootcamp.getbootCamp().getBootcampId())
+                        .totalCost(mybootcamp.getbootCamp().getTotalCost())
+                        .title(mybootcamp.getbootCamp().getTitle())
+                        .process(mybootcamp.getbootCamp().getProcess())
+                        .onOff(mybootcamp.getbootCamp().getOnOff())
+                        .beginRegisterDate(mybootcamp.getbootCamp().getBeginRegisterDate())
+                        .finalRegisterDate(mybootcamp.getbootCamp().getFinalRegisterDate())
                         .build())
                 .collect(Collectors.toList());
     }
