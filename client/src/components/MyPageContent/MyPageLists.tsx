42964e524ab13a838454e3d0d53dc304d93a34b4
skyhanull <anky0425@naver.com>
2022-12-01 09:48:07 +0900

    docs:데이터타입추가

--- client/src/components/MyPageContent/MyPageLists.tsx
+++ client/src/components/MyPageContent/MyPageLists.tsx
@@ -4,37 +4,53 @@ import { formatDistanceToNow } from 'date-fns';
 import { ko } from 'date-fns/locale';
 
 interface PropsType {
-  list: {
-    postscriptId?: number;
-    postscriptTitle: string;
-    postscriptContent: string;
-    view: number;
-    like: number;
-    user: {
-      userId: number;
-      name: string;
-      userEmail: string;
-    };
-    createdAt: string;
-    updatedAt?: string;
-  };
+  mentoringId?: number;
+  mentoringTitle?: string;
+  mentoringContent?: string;
+  postscriptId?: number;
+  postscriptTitle?: string;
+  postscriptContent?: string;
+  studyId?: number;
+  studyTitle?: string;
+  studyContent?: string;
+  totalVotes: number;
+  createdAt: string;
+  updatedAt: string;
+  tagName: string;
+  view: number;
 }
 
-const MyPageLists = ({ list }: PropsType) => {
-  const createTime = formatDistanceToNow(new Date(list.createdAt), { locale: ko });
-  const updateTime = formatDistanceToNow(new Date(list.updatedAt ? list.updatedAt : new Date()), { locale: ko });
+const MyPageLists = ({
+  studyId,
+  studyTitle,
+  studyContent,
+  postscriptId,
+  postscriptTitle,
+  postscriptContent,
+  mentoringId,
+  mentoringTitle,
+  mentoringContent,
+  totalVotes,
+  createdAt,
+  updatedAt,
+  tagName,
+  view,
+}: PropsType) => {
+  const createTime = formatDistanceToNow(new Date(createdAt), { locale: ko });
+  const updateTime = formatDistanceToNow(new Date(updatedAt ? updatedAt : new Date()), { locale: ko });
   return (
     <S.PostMyPageContent>
-      <S.PostLinkMyPage to={`/postscript/${list.postscriptId}`}>
-        <S.MyPageListsTitle>{list.postscriptTitle}</S.MyPageListsTitle>
-        <S.MyPageListsContent>{list.postscriptContent}</S.MyPageListsContent>
+      <S.PostLinkMyPage
+        to={postscriptId ? `/postscript/${postscriptId}` : studyId ? `/study/${studyId}` : `/mentoring/${mentoringId}`}
+      >
+        <S.MyPageListsTitle>{studyTitle || mentoringTitle || postscriptTitle}</S.MyPageListsTitle>
+        <S.MyPageListsContent>{studyContent || mentoringContent || postscriptContent}</S.MyPageListsContent>
         <S.PostInfoView>
           <S.UserLikeIconList>
-            {list.view ? <span>조회{list.view}</span> : null}
-            {list.updatedAt ? <div> {updateTime}전</div> : <div> {createTime}전</div>}
+            {view ? <span>조회{view}</span> : null}
+            {updatedAt ? <div> {updateTime}전</div> : <div> {createTime}전</div>}
             <div>
-              <InlineIcon icon="akar-icons:heart" />
-              {list.like}
+              <InlineIcon icon="akar-icons:heart" />a
             </div>
           </S.UserLikeIconList>
         </S.PostInfoView>
