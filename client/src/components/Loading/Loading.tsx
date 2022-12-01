3a6bcc7ac9d8b58c33ff8e95149037f03ae12813
headring <myhakiki@gmail.com>
2022-12-01 14:38:18 +0900

    feat: 임시로 loading 컴포넌트 생성

---
+++ client/src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="loading">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt="loading" />
+    </div>
+  );
+};
+
+export default Loading;
