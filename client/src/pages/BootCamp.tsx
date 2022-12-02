d47115673d8d49ade7d244117a022c9dced35693
Jongwon Park <jwp7073@gmail.com>
2022-12-02 15:08:45 +0900

    fix: BootCamp 페이지에서 발생하던 FilterButton 관련 오류 수정

--- client/src/pages/BootCamp.tsx
+++ client/src/pages/BootCamp.tsx
@@ -15,7 +15,7 @@ const BootCamp = () => {
       <S.MiddleSection>
         <div>
           <SearchBar />
-          <FilterButton url="url" setPosts={setPosts} />
+          <FilterButton pageType="bootcamp" url={`/bootcamp?page=1&size=10`} setPosts={setPosts} />
         </div>
       </S.MiddleSection>
       <section>
