9bba60944657f114033fe7ab045c40fe5c84d511
headring <myhakiki@gmail.com>
2022-12-03 05:18:30 +0900

    comment: 다른 branch 작업 사항 기재

--- client/src/components/Route/PrivateRoute.tsx
+++ client/src/components/Route/PrivateRoute.tsx
@@ -1,14 +1,15 @@
-import { Navigate } from 'react-router-dom';
-// import { useRecoilValue } from 'recoil';
-// import { isLogin } from '../atoms';
+import { Navigate, Route, useLocation } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { logUser } from '../../atoms';
 
-// 로그인 저장 정보 확립 시 재수정 필요
-// const status = useRecoilValue(isLogin);
-
-const status = false;
+// 로그인 저장 정보 확립 시 재수정 필요 - 다른 branch로 작업 예정
+const { isLog } = useRecoilValue(logUser);
 
 const PrivateRoute = ({ children }: any) => {
-  return !status ? <Navigate to="/users/login" /> : children;
+  // const { pathname } = useLocation();
+  // console.log(pathname);
+
+  return !isLog ? <Navigate to="/users/login" /> : children;
 };
 
 export default PrivateRoute;
