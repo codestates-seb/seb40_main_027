08c08a1cd6eb556bf3c202f1dd39c8f52e8bce18
skyhanull <anky0425@naver.com>
2022-12-02 10:53:37 +0900

    feat: 찜버튼 구현

---
+++ client/src/components/MyPageContent/BootLikeButton.tsx
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import * as S from './MyPageTable.style';
+import { Icon } from '@iconify/react';
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+export const BootLikeButton = () => {
+  const [likeBtn, setLikeBtn] = useState(1);
+  const access = localStorage.getItem('access');
+  const { id } = useParams();
+
+  const handleCancel = (event: any) => {
+    event.stopPropagation();
+  };
+
+  const getBtn = () => {
+    return axios({
+      method: 'get',
+      url: '/users/mypage/bootcampLike',
+      headers: {
+        Authorization: access,
+      },
+    });
+  };
+
+  const postLikeBtn = (idx: number) => {
+    return axios({
+      method: 'post',
+      url: `/bootcamp/votes/${id}?${idx}`,
+      headers: {
+        Authorization: access,
+      },
+    });
+  };
+  const aysncFunc = async () => {
+    await postLikeBtn(0);
+    const getasync = await getBtn();
+  };
+
+  const likeButtonHandler = (e: any, idx: number) => {
+    aysncFunc();
+    handleCancel(e);
+  };
+
+  return (
+    <div>
+      {likeBtn === 1 ? (
+        <span>
+          <S.LikeButton type="button" onClick={(event) => handleCancel(event)}>
+            <Icon icon="ic:round-star" color="#fbb3b3" width="30" height="30" />
+          </S.LikeButton>
+        </span>
+      ) : (
+        <Icon icon="ic:round-star-border" color="#ff4343" width="30" height="30" />
+      )}
+    </div>
+  );
+};
