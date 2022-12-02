da7cc88b5286f59d81bdaced89c46674a7ae82b8
headring <myhakiki@gmail.com>
2022-12-03 04:12:57 +0900

    style: 모바일 환경에서의 가시성 및 위치 수정

--- client/src/components/Table/Table.style.ts
+++ client/src/components/Table/Table.style.ts
@@ -1,10 +1,12 @@
+import { ContentWrapper } from './../../pages/Forum.style';
 import styled from 'styled-components';
 import theme from '../../styles/theme';
+import { Link } from 'react-router-dom';
 
 export const TableWrap = styled.table`
   width: 60vw;
   min-width: 960px;
-  font-size: 20px;
+  font-size: 1.2vw;
   thead {
     font-weight: bold;
     background-color: var(--greenSub);
@@ -18,13 +20,14 @@ export const TableWrap = styled.table`
     }
   }
   tbody {
-    tr {
+    a {
+      text-decoration: none;
+    }
+    td {
+      padding: calc(100vh / 40) 0;
       border: 1px solid var(--grayTableBorder);
-      td {
-        padding: calc(100vh / 40) 0;
-        border: 1px solid var(--grayTableBorder);
-        text-align: center;
-      }
+      text-align: center;
+      font-size: 1rem;
     }
   }
   @media ${theme.mobile} {
@@ -36,10 +39,17 @@ export const MobileComp = styled.div`
   display: none;
 
   @media ${theme.mobile} {
-    display: block;
-    width: 97vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90vw;
+    height: 160px;
     > div {
+      width: 100%;
+      height: 100%;
       display: flex;
+      justify-content: center;
+      align-items: center;
       border: 1px solid var(--grayTableBorder);
       padding: calc(100% / 20) 0;
     }
@@ -56,7 +66,8 @@ export const MobileLeft = styled.div`
   }
 `;
 
-export const MobileMiddle = styled.div`
+export const MobileMiddle = styled(Link)`
+  text-decoration: none;
   width: calc(100% / 3);
   display: flex;
   flex-direction: column;
@@ -67,10 +78,13 @@ export const MobileMiddle = styled.div`
 `;
 export const MobileRight = styled.div`
   width: calc(100% / 3);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   div {
     padding: calc(100% / 15) 0;
+    > div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
   }
 `;
