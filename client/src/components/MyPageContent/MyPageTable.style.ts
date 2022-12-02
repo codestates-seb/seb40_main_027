55252d38fb998519f3cb9d5294a26b10d2ab0a97
skyhanull <anky0425@naver.com>
2022-12-02 23:41:24 +0900

    design:css변경

--- client/src/components/MyPageContent/MyPageTable.style.ts
+++ client/src/components/MyPageContent/MyPageTable.style.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const TableSchedule = styled.table`
-  height: 50%;
   width: 100%;
 
   thead {
@@ -19,9 +18,11 @@ export const TableSchedule = styled.table`
   }
   tbody {
     td {
-      border-bottom: 1px solid var(--grayContentsBorder);
       text-align: center;
       padding: calc(100vh / 40) 0;
+    }
+    tr {
+      border-bottom: 1px solid var(--grayContentsBorder);
     }
     tr:nth-child(even) {
       background: var(--grayBoardAreaBorder);
@@ -41,6 +42,7 @@ export const TableSchedule = styled.table`
         border-bottom: 1px solid var(--grayContentsBorder);
         text-align: center;
         padding: calc(100vh / 40) 0;
+        overflow: hidden;
       }
     }
   }
@@ -54,5 +56,8 @@ export const TableLink = styled(Link)`
 
 export const LikeButton = styled.button`
   background-color: rgba(0, 0, 0, 0);
+  display: flex;
+  align-items: center;
+  height: 100%;
   margin: 0;
 `;
