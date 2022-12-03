6b3ed77ad48f50d1023a0bd017c1d62cc06df1ae
headring <myhakiki@gmail.com>
2022-12-04 02:11:17 +0900

    refacotr: 로딩 컴포넌트 div 클래스이름 삭제

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="loading">
+    <div>
       <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt="loading" />
     </div>
   );
