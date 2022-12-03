26beb98f238ab2e755af5c2f6587a3ed2a0bbc24
pjnw1236 <pjnw1236@gmail.com>
2022-12-03 11:07:53 +0900

    fix: cors config

--- server/src/main/java/com/yes27/config/CorsConfig.java
+++ server/src/main/java/com/yes27/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedHeaders("*")
-            .allowedMethods("*");
+            .allowedMethods("*")
+            .exposedHeaders("Authorization", "Refresh");
     }
 }
