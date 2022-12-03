284c656424a87618e0cc605cd2b1f2c77a4fc05a
pjnw1236 <pjnw1236@gmail.com>
2022-12-03 10:10:11 +0900

    feat: Cors setting

---
+++ server/src/main/java/com/yes27/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.yes27.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .allowedMethods("*");
+    }
+}
